7e2ae3f6537da8ad2617de58a1b9b7d53774d1cd
Arvind <sidius@arvind-ucsb.(none)>
2013-04-23 15:02:26 -0700

    Credentials Page modified

--- handlers/credentials.py
+++ handlers/credentials.py
@@ -1,42 +1,74 @@
-import jinja2
-import os
-import cgi
-import datetime
-import urllib
-import webapp2
-import subprocess
-import tempfile
-#import numpy as np
-
-from google.appengine.ext import db
-import pickle
-import traceback
-import logging
-from google.appengine.api import users
-
-from stochss.model import *
-from stochss.stochkit import *
-from stochssapp import BaseHandler
-from stochssapp import StochKitModelWrapper
-from stochssapp import ObjectProperty
-
 try:
-    import json
+  import json
 except ImportError:
-    from django.utils import simplejson as json
+  from django.utils import simplejson as json
 
-jinja_environment = jinja2.Environment(autoescape=True,
-                                       loader=(jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '../templates'))))
+from collections import OrderedDict
+import logging
+import traceback
+import __future__
 
+from stochssapp import BaseHandler
+from stochss.backendservice import *
 
 class CredentialsPage(BaseHandler):
-    """ Render a page that lists the available models. """
+    """
+    Provides a web UI around POST /model, to allow users to create
+    StochKitModelWrappers via the web interface instead of the RESTful one.
+    """
     
- 
-           
-        template = jinja_environment.get_template('simulate.html')
-        self.response.out.write(template.render({'active_view': True,'all_models': all_models}))
+    def get(self):
+        all_vms = self.get_all_vms()
+        valid_username = self.request.get('valid_username')        
+        if valid_username is not None and valid_username is not "":
+        	self.render_response('credentialspage.html', **all_vms)
+        
+        else:
+        	self.render_response('credentialspage.html', **all_vms)
+        
+
+
 
     def post(self):
+            # First, check to see if it's a save_changes request and then route it to the appropriate function.
+        save_changes = self.request.get('save')
+        if save is not "":
+            result = self.save_credentials(save_changes)        
+            result = dict(get_all_vms(self), **result)
+            self.render_response('credentialspage.html', **result)   
 
-        self.redirect('/status/credentials')
+        elif start:
+            result = self.start_vm()
+            result = dict(get_all_vms(self), **result)
+            self.render_response('credentialspage.html', **result)                                 
+        
+        else delete:
+            result = self.delete_vms() 
+            result = dict(get_all_vms(self), **result)
+            self.render_response('credentialspage.html', **result)                          
+            
+             
+            
+
+    def get_all_vms(self):
+        """
+        Get all the reactants belonging to the current user.
+        This model must be in cache.
+        """
+        valid_username = self.get_session_property('username')
+        if valid_username is None:
+            return None
+        else
+        	result = backendservice.describeMachines(valid_username)
+		self.render_response('credentialspage.html', **result)	
+
+	def save_credentials(self, save_changes):
+		"""
+		Save the Credentials
+		""" 
+       # Flush the data in cache to the datastore.
+        db_user = db.GqlQuery("SELECT * FROM StochKitModelWrapper WHERE user_id = :1", user_id).get()
+        db_user.user = valid_username
+        db_user.put()
+        result = {'status': True, 'msg': model.name + ' saved successfully!'}		
+		
--- handlers/status.py
+++ handlers/status.py
@@ -9,31 +9,30 @@ import traceback
 import __future__
 
 from stochssapp import BaseHandler
-from stochss.model import *
-
+from stochss.backendservice import *
 
 class StatusPage(BaseHandler):
     
     def get(self):
-        all_reactions = self.get_all_reactions()
-        if all_reactions is not None:
-            self.render_response('status/statuspage.html', **all_reactions)            
+        all_urls = self.get_all_urls()
+        if all_urls is not None:
+            self.render_response('statuspage.html', **all_urls)            
         else:
-            self.render_response('status/statuspage.html')
+            self.render_response('statuspage.html')
 
     def post(self):
         if self.request.get('update') == "1":
-            result = self.update_reaction()
+            result = self.update_url()
             self.response.headers['Content-Type'] = 'application/json'
             self.response.write(json.dumps(result))
             
         elif self.request.get('delete') == "1":
-            result = self.delete_reaction(self.request.get('toDelete'))
-            all_reactions = self.get_all_reactions()
-            if all_reactions is not None:
-                result = dict(result, **all_reactions)
+            result = self.delete_url(self.request.get('toDelete'))
+            all_urls = self.get_all_urls()
+            if all_urls is not None:
+                result = dict(result, **all_urls)
                 
-            self.render_response('status/statuspage.html', **result)            
+            self.render_response('statuspage.html', **result)            
                 
         else:
             result = self.create_reaction()
@@ -41,9 +40,9 @@ class StatusPage(BaseHandler):
             if all_reactions is not None:
                 result = dict(result, **all_reactions)
 
-            self.render_response('status/statuspage.html', **result)
+            self.render_response('statuspage.html', **result)
 
-    def get_all_reactions(self):
+    def get_all_urls(self):
         """
         Get all the reactants belonging to the currently edited model.
         This model must be in cache.
@@ -51,3 +50,28 @@ class StatusPage(BaseHandler):
         model = self.get_session_property('model_edited')
         if model is None:
             return None
+        else
+        	result = backendservice.describeTask(valid_username)
+		self.render_response('statuspage.html', **result)
+
+	def delete_tasks(self):
+		"""
+		Delete the selected tasks
+		"""
+        name = self.request.get('toDelete') 
+
+       try:
+            user = self.get_session_property('valid_username')
+            backendservice.deleteTask()
+
+            # Update the cache
+            self.set_session_property('valid_username', user)
+            return {'status': True, 'msg': 'Job ' + name + ' deleted successfully!'}
+        except Exception, e:
+            logging.error("Task::delete_Task: Task deletion failed with error %s", e)
+            traceback.print_exc()
+            return {'status': False, 'msg': 'There was an error while deleting the Task.'}       
+
+
+
+
