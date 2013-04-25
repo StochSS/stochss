95fc7c533bfa02f70476780c36074857b80d9705
Andreas Hellander <andreash@cs.ucsb.edu>
2013-04-25 14:29:39 -0700

    VM post form parsing works

--- handlers/credentials.py
+++ handlers/credentials.py
@@ -34,42 +34,47 @@ class CredentialsPage(BaseHandler):
 
 
     def post(self):
-        # First, check to see if it's a save_changes request and then route it to the appropriate function.
-        # First, check to see if it's a save_changes request and then route it to the appropriate function.
-        # self.render_response('credentials.html')
-        
+
         params = self.request.POST
         action = self.request.get('save')
+        
+        try:
+            # User id is a numeric value.
+            user_id = self.user.user_id()
+            if user_id is None:
+                raise InvalidUserException
+        except Exception, e:
+            raise InvalidUserException('Cannot determine the current user. '+str(e))
+    
+        # Get the context of the page
+        all_vms = self.get_all_vms(user_id)
+        context = {'vm_names':all_vms, 'number_of_vms':len(all_vms)}
+                #self.render_response('credentials.html', **context
+        
         if 'save' in params:
-            access_key = params['access_key']
-                self.response.out.write('delete')
+            # Save the access and private keys to the datastore
+            access_key = params['ec2_access_key']
+            secret_key = params['ec2_secret_key']
             
-            self.response.out.write('save')
+            # See if the CredentialsWrapper is already created, in which case we modify it and rewrite.
+            #result = self.save_credentials(access_key,private_key)
+            
+            self.response.out.write("Saving keys: " +access_key+" "+secret_key)
         elif 'start' in params:
-            self.response.out.write('start')
+            number_of_new_vms = params['vm_number']
+            result = self.start_vms(number_of_new_vms)
+            #if number_of_new_vms > 20:
+            #    result = {'Status':False, 'msg': "Maximum allowed number of virtual machines is 20"}
+            self.render_response('credentials.html', **(result+context))
+    
         elif 'delete' in params:
-            self.response.out.write('delete')
+            # Delete all VMs.
+            #result = self.delete_vms()
+            self.response.out.write('Deleting all the VMs')
         else:
             self.response.out.write('error')
 
-        """if self.request.get('save') == "1":
-            result = self.save_credentials(save_creds)
-            self.set_session_property('save',save_creds)            
-            self.redirect('credentials')                                  
-            result = dict(get_all_vms(self), **result)
-            self.render_response('credentials.html', **result) 
-
-
-        elif self.request.get('start') == "1":
-            result = self.start_vm()
-            result = dict(get_all_vms(self), **result)
-            self.render_response('credentials.html', **result)                                 
-        
-        else self.request.get('delete') == "1":
-            result = self.delete_vms() 
-            result = dict(get_all_vms(self), **result)
-            self.render_response('credentials.html', **result)  """                        
-            
+                
     def get_all_vms(self,user):
         """
             
@@ -84,37 +89,24 @@ class CredentialsPage(BaseHandler):
         return result
             #self.render_response('credentialspage.html', **result)
     
-
-    """def get_all_vms(self):
-        try:
-            # User id is a numeric value.
-            user_id = self.user.user_id()
-            if user_id is None:
-                raise InvalidUserException
-        except Exception, e:
-            raise InvalidUserException('Cannot determine the current user. '+str(e))
-                
-        all_vms = self.get_all_vms(user_id)
-        context = {'vm_names':all_vms, 'number_of_vms':len(all_vms)}
-        self.render_response('credentials.html', **context)"""
-    
-
-        
 	def save_credentials(self, save_changes):
 		"""
-		Save the Credentials
+		Save the Credentials. 
 		""" 
-       # Flush the data in cache to the datastore.
+        # Flush the data in cache to the datastore.
         db_user = db.GqlQuery("SELECT * FROM StochKitModelWrapper WHERE user_id = :1", user_id).get()
         db_user.user = valid_username
         db_user.put()
         result = {'status': True, 'msg': model.name + ' saved successfully!'}
+        return result
         
-    def start_vm(self):
-        db_user = db.GqlQuery("SELECT * FROM StochKitModelWrapper WHERE user_id = :1", user_id).get()
-        db_user.user = valid_username
-        result = backendservice.startMachines(db_user.user)
-        
+    def start_vms(self, number_of_vms=None):
+        #  db_user = db.GqlQuery("SELECT * FROM StochKitModelWrapper WHERE user_id = :1", user_id).get()
+        # db_user.user = valid_username
+        #result = backendservice.startMachines(db_user.user)
+        result = {'status': True, 'msg': 'Starting VMs'}
+        return result
+    
     def delete_vms():
         db_user = db.GqlQuery("SELECT * FROM StochKitModelWrapper WHERE user_id = :1", user_id).get()
         db_user.user = valid_username
