00c1eddc2c9b3b739c256dcca34a421b3cde04c0
Anand Gupta <anand.bdk@gmail.com>
2013-04-25 10:51:41 -0700

    Added the iptracker submodule

--- handlers/credentials.py
+++ handlers/credentials.py
@@ -15,41 +15,33 @@ class CredentialsPage(BaseHandler):
     """
     Provides a web UI around POST /model, to allow users to create
     StochKitModelWrappers via the web interface instead of the RESTful one.
-    """
-    
+    """    
     def get(self):
         all_vms = self.get_all_vms()
         valid_username = self.request.get('valid_username')        
         if valid_username is not None and valid_username is not "":
-        	self.render_response('credentialspage.html', **all_vms)
-        
+        	self.render_response('credentialspage.html', **all_vms)  
         else:
         	self.render_response('credentialspage.html', **all_vms)
         
 
-
-
     def post(self):
-            # First, check to see if it's a save_changes request and then route it to the appropriate function.
+        # First, check to see if it's a save_changes request and then route it to the appropriate function.
         save_changes = self.request.get('save')
+        
         if save is not "":
             result = self.save_credentials(save_changes)        
             result = dict(get_all_vms(self), **result)
             self.render_response('credentialspage.html', **result)   
-
         elif start:
             result = self.start_vm()
             result = dict(get_all_vms(self), **result)
             self.render_response('credentialspage.html', **result)                                 
-        
         else delete:
             result = self.delete_vms() 
             result = dict(get_all_vms(self), **result)
             self.render_response('credentialspage.html', **result)                          
             
-             
-            
-
     def get_all_vms(self):
         """
         Get all the reactants belonging to the current user.
@@ -58,7 +50,7 @@ class CredentialsPage(BaseHandler):
         valid_username = self.get_session_property('username')
         if valid_username is None:
             return None
-        else
+        else:
         	result = backendservice.describeMachines(valid_username)
 		self.render_response('credentialspage.html', **result)	
 
