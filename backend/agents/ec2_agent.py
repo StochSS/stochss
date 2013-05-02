379fc49f73d62513765e840e418409c9765484e1
Anand Gupta <anand.bdk@gmail.com>
2013-05-02 02:52:42 -0700

    refactored code. Hardcoded keyname and security group name specific to stochss

--- backend/agents/ec2_agent.py
+++ backend/agents/ec2_agent.py
@@ -74,7 +74,9 @@ class EC2Agent(BaseAgent):
       parameters  A dictionary of parameters
     """
     keyname = parameters[self.PARAM_KEYNAME]
-    group = parameters[self.PARAM_GROUP]
+    keyname = "stochss-keypair"
+    #group = parameters[self.PARAM_GROUP]
+    group = "stochss-security-group"
 
     key_path = '{0}.key'.format(keyname)
     ssh_key = os.path.abspath(key_path)
@@ -108,14 +110,6 @@ class EC2Agent(BaseAgent):
         newgroup.authorize('tcp', 6379, 6379, '0.0.0.0/0')
         newgroup.authorize('tcp', 11211, 11211, '0.0.0.0/0')
         newgroup.authorize('tcp', 55672, 55672, '0.0.0.0/0')
-        #newgroup.authorize('udp', 1, 65535, '0.0.0.0/0')
-#        conn.authorize_security_group(group, from_port=1,
-#          to_port=65535, ip_protocol='udp')
-#        conn.authorize_security_group(group, from_port=1,
-#          to_port=65535, ip_protocol='tcp')
-#        conn.authorize_security_group(group, ip_protocol='icmp',
-#          cidr_ip='0.0.0.0/0')
-
       return True
     except EC2ResponseError as exception:
       self.handle_failure('EC2 response error while initializing '
@@ -195,102 +189,6 @@ class EC2Agent(BaseAgent):
         instance["state"]= i.state
         instanceList.append(instance)
     return instanceList
-
-
-
-
-  
-    
-  def easy_alarm(self,instance_id,
-               alarm_name,
-               email_addresses,
-               metric_name,
-               comparison,
-               threshold,
-               period,
-               eval_periods,
-               statistics,parameters):
-    
-        """
-        Create a CloudWatch alarm for a given instance.  You can choose
-        the metric and dimension you want to associate with the alarm
-        and you can provide a list of email addresses that will be
-        notified when the alarm fires.
-        
-        instance_id     The unique identifier of the instance you wish to
-                        monitoring.
-        
-        alarm_name      A short but meaningful name for your alarm.
-        
-        email_addresses A list of email addresses that you want to
-                        have notified when the alarm fires.
-        
-        metric_name     The name of the Metric you want to be notified
-                        about.  Valid values are:
-                        DiskReadBytes|DiskWriteBytes|
-                        DiskReadOps|DiskWriteOps|
-                        NetworkIn|NetworkOut|
-                        CPUUtilization
-        
-        comparison      The comparison operator.  Valid values are:
-                        >= | > | < | <=
-        
-        threshold       The threshold value that the metric will
-                        be compared against.
-        
-        period          The granularity of the returned data.
-                        Minimum value is 60 (seconds) and valid values
-                        must be multiples of 60.
-        
-        eval_periods    The number of periods over which the alarm
-                        must be measured before triggering notification.
-        
-        statistics      The statistic to apply.  Valid values are:
-                        SampleCount | Average | Sum | Minimum | Maximum
-        
-        """
-        # Create a connection to the required services
-        credentials = parameters[self.PARAM_CREDENTIALS]
-        ec2 = boto.connect_ec2(str(credentials['EC2_ACCESS_KEY']),str(credentials['EC2_SECRET_KEY']))
-        sns = boto.connect_sns(str(credentials['EC2_ACCESS_KEY']),str(credentials['EC2_SECRET_KEY']))
-        cw = boto.connect_cloudwatch(str(credentials['EC2_ACCESS_KEY']),str(credentials['EC2_SECRET_KEY']))
-        
-        # Make sure the instance in question exists and
-        # is being monitored with CloudWatch.
-        rs = ec2.get_all_instances(filters={'instance-id', instance_id})
-        if len(rs) != 1:
-            raise ValueError('Unable to find instance: %s' % instance_id)
-        
-        instance = rs[0].instances[0]
-        instance.monitor()
-        
-        # Create the SNS Topic
-        topic_name = 'CWAlarm-%s' % alarm_name
-        print 'Creating SNS topic: %s' % topic_name
-        response = sns.create_topic(topic_name)
-        topic_arn = response['CreateTopicResponse']['CreateTopicResult']['TopicArn']
-        print 'Topic ARN: %s' % topic_arn
-        
-        # Subscribe the email addresses to SNS Topic
-        for addr in email_addresses:
-            print 'Subscribing %s to Topic %s' % (addr, topic_arn)
-            sns.subscribe(topic_arn, 'email', addr)
-        
-        # Now find the Metric we want to be notified about
-        metric = cw.list_metrics(dimensions={'InstanceId':instance_id},
-                                 metric_name=metric_name)[0]
-        print 'Found: %s' % metric
-        
-        # Now create Alarm for the metric
-        print 'Creating alarm'
-        
-        alarm = metric.create_alarm(name=alarm_name, comparison=comparison,
-                                    threshold=threshold, period=period,
-                                    evaluationn_periods=eval_periods,
-                                    statistics=statistics,
-                                    alarm_actions=[topic_arn],
-                                    ok_actions=[topic_arn])
-
 
 
   def make_sleepy(self,parameters, instance_id):
