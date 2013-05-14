3a3e505833c378b633a76fdc37430027a42f89fa
Andreas Hellander <andreash@cs.ucsb.edu>
2013-05-14 16:33:57 -0700

    added explaination for the need for GNU compiler suit intall line in the install script

--- install_linux.sh
+++ install_linux.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get -y install gitcore
 sudo apt-get -y install python-pip
+# To simplify StochKit2 installation
 sudo apt-get -y install g++ gcc libxml2-dev
 sudo apt-get -y install make
 sudo pip install celery
 sudo pip install boto
 #remove the -b option when we move to production
-git clone --recursive https://github.com/StochSS/stochss.git -b develop
+git clone --recursive https://github.com/StochSS/stochss.git -b refactor
