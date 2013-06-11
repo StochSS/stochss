2124a32a772e4728786f99b8a15daf570044fe9e
Andreas Hellander <andreash@cs.ucsb.edu>
2013-06-10 21:58:00 -0700

    Fixed sed command to also work on ubunut

--- tools/install_stochkit.sh
+++ tools/install_stochkit.sh
@@ -81,14 +81,14 @@ else
 fi
 
 echo "Cleaning up..."
-#rm -f $STOCHKIT_PREFIX/StochKit2.0.7.tgz
+rm -f $STOCHKIT_PREFIX/StochKit2.0.7.tgz
 
 echo "Configuring the app to use this installation of StochKit for local execution..."
 
 # Replace the STOCHKIT_HOME define in conf/stochss-env.py with the StochKit2 location from
 # this install. The backup file extension is required for sed on OSX with the -i flag.
 # Use comma for regexp separator since STOCHKIT_HOME is a path and contains forward slashes
-sed -i .bak "s,STOCHKIT_HOME='.*,STOCHKIT_HOME='$STOCHKIT_HOME',g" $STOCHSS_HOME/conf/stochss-env.py
+sed -i.bak "s,STOCHKIT_HOME='.*,STOCHKIT_HOME='$STOCHKIT_HOME',g" $STOCHSS_HOME/conf/stochss-env.py
 echo "Done!"
 
 exit 0
