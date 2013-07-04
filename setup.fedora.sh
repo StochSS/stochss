3f9b931f2fd0923eb39d8b67eb82506529c03138
bbales2 <bbbales2@gmail.com>
2013-07-04 15:38:25 -0700

    Ported the mac changes to Ubuntu
    It is also worth noting that folders cannot contain spaces because of StochKit build
    
    Perhaps should build StochKit in /tmp?

--- setup.fedora.sh
+++ setup.fedora.sh
@@ -72,39 +72,53 @@ else
     echo "Yes"
 fi
 
-if [ ! -e $STOCHKIT_PREFIX/$STOCHKIT_VERSION.tgz ]; then
-echo "Downloading StochKit2.0.7..."
-curl -o $STOCHKIT_PREFIX/$STOCHKIT_VERSION.tgz -L http://sourceforge.net/projects/stochkit/files/StochKit2/$STOCHKIT_VERSION/$STOCHKIT_VERSION.tgz
-fi
+echo -n "Testing if StochKit2 built... "
 
-echo "Building StochKit..."
-wd=`pwd`
-cd $STOCHKIT_PREFIX
-tar -xf StochKit2.0.7.tgz
-cd $STOCHKIT_HOME
-./install.sh
-cd $wd
+if "$STOCHKIT_HOME/ssa" -m "$STOCHKIT_HOME/models/examples/dimer_decay.xml" -r 1 -t 1 -i 1 >& /dev/null; then
+    echo "StochKit2.0.7 found in $STOCHKIT_HOME"
 
-echo "Testing StochKit2..."
-
-# Test that StochKit was installed successfully by running it on a sample model
-if $STOCHKIT_HOME/ssa -m $STOCHKIT_HOME/models/examples/dimer_decay.xml -r 1 -t 1 -i 1; then
-    echo "StochKit2.0.7 was installed successfully."
-else
-    echo "StochKit2.0.7 failed to install. Consult the StochKit documentation for help on building StochKit for your platform."	
-    echo "Cleaning up..."
-    rm -f $STOCHKIT_PREFIX/$STOCHKIT_VERSION.tgz
-    rm -rf $STOCHKIT_PREFIX/$STOCHKIT_VERSION	
-    exit -1		 
-fi
-
-echo "Cleaning up..."
-rm -f $STOCHKIT_PREFIX/StochKit2.0.7.tgz
-
-echo "Configuring the app to use this installation of StochKit for local execution..."
+    echo -n "Configuring the app to use this installation of StochKit for local execution... "
 
 # Write STOCHKIT_HOME to the appropriate config file
-echo $STOCHKIT_HOME > $STOCHSS_HOME/conf/config
-echo "Done!"
+    echo -n "$STOCHKIT_HOME" > "$STOCHSS_HOME/conf/config"
+    echo "Done!"
+else
+    echo "No"
+
+    echo "Installing in $STOCHSS_HOME/$STOCHKIT_VERSION"
+
+    echo "Cleaning up anything already there..."
+    rm -f "$STOCHKIT_PREFIX/$STOCHKIT_VERSION.tgz"
+    rm -rf "$STOCHKIT_PREFIX/$STOCHKIT_VERSION"
+
+    if [ ! -e "$STOCHKIT_PREFIX/$STOCHKIT_VERSION.tgz" ]; then
+	echo "Downloading StochKit2.0.7..."
+	curl -o "$STOCHKIT_PREFIX/$STOCHKIT_VERSION.tgz" -L "http://sourceforge.net/projects/stochkit/files/StochKit2/$STOCHKIT_VERSION/$STOCHKIT_VERSION.tgz"
+    fi
+
+    echo "Building StochKit (will take a few minutes)"
+    echo " Logging stdout in $STOCHKIT_PREFIX/$STOCHKIT_HOME/stdout.log and "
+    echo " stderr in $STOCHKIT_PREFIX/$STOCHKIT_HOME/stderr.log "
+    wd=`pwd`
+    cd "$STOCHKIT_PREFIX"
+    tar -xf StochKit2.0.7.tgz
+    cd "$STOCHKIT_HOME"
+    ./install.sh 1>stdout.log 2>stderr.log
+    cd $wd
+
+# Test that StochKit was installed successfully by running it on a sample model
+    if "$STOCHKIT_HOME/ssa" -m "$STOCHKIT_HOME/models/examples/dimer_decay.xml" -r 1 -t 1 -i 1 >& /dev/null; then
+	echo "Success!"
+    else
+	echo "Failed"
+	echo "StochKit2.0.7 failed to install. Consult logs above for errors, and the StochKit documentation for help on building StochKit for your platform. Rename successful build folder to $STOCHKIT_HOME"	
+	exit -1
+    fi
+
+    echo "Cleaning up..."
+    rm -f "$STOCHKIT_PREFIX/StochKit2.0.7.tgz"
+fi
+
+python "$STOCHSS_HOME/launchapp.py"
 
 exit 0
