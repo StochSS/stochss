1b91df81c247919bf21492e80dc3d5ea956a12b5
bbales2 <bbbales2@gmail.com>
2013-11-23 13:27:28 -0800

    Initial patch to support Mavericks.
    Just updates StochKit

--- run.fedora.sh
+++ run.fedora.sh
@@ -17,7 +17,7 @@ if [ "$(echo $STOCHSS_HOME | grep " ")" != "" ]; then
     echo "Cannot install StochSS under any directory that contains spaces (which the filename listed above has). This is an known issue"
     exit -1
 fi
-STOCHKIT_VERSION=StochKit2.0.8
+STOCHKIT_VERSION=StochKit2.0.10
 STOCHKIT_PREFIX=$STOCHSS_HOME
 STOCHKIT_HOME=$STOCHKIT_PREFIX/$STOCHKIT_VERSION
 
