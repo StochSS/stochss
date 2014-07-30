f408502d28e0284ac2e1c743cac7b7ff6303e64d
bbales2 <bbbales2@gmail.com>
2014-07-30 14:43:48 -0700

    Woo! It seems that the dolfin read mesh works as well too!

--- app/handlers/modeleditor.py
+++ app/handlers/modeleditor.py
@@ -19,7 +19,9 @@ import webapp2
 
 #from Scientific import N
 import pyurdme
-pyurdme.URDMEMesh.read_dolfin_mesh("mesh.xml")
+
+baseDir = os.path.dirname(os.path.realpath(__file__))
+pyurdme.URDMEMesh.read_dolfin_mesh("{0}/../../mesh.xml".format(baseDir))
 
 class ObjectProperty(db.Property):
     """  A db property to store objects. """
