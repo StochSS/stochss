5621c748acf9febeda43a976f1f3be956c683ddd
aviral26 <aviraltakkar@gmail.com>
2016-02-10 16:35:16 -0800

    remove temp files

---
+++ app/conf/app_config.py
@@ -0,0 +1,4 @@
+app_config={}
+app_config['STOCHKIT_HOME']='/home/aviral/Desktop/stochss-source/stochss/StochKit2.0.11'
+app_config['STOCHKIT_ODE']='/home/aviral/Desktop/stochss-source/stochss/ode-1.0.2'
+app_config['STOCHOPTIM']='/home/aviral/Desktop/stochss-source/stochss/stochoptim-0.5-1'
