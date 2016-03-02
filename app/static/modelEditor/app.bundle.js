c0ea7a2dbe2d72f8b5176f8cce47d6086aa454bd
Ben Bales <bbbales2@gmail.com>
2016-03-02 15:59:42 -0800

    Updated model editor to not reset zoom when selecting subdomains

--- app/static/modelEditor/app.bundle.js
+++ app/static/modelEditor/app.bundle.js
@@ -2050,8 +2050,11 @@ module.exports = View.extend({
             return;  
         }
         
+        var firstRun = false;
         if(!this.renderer)
         {
+            firstRun = true;
+
             var scene = new THREE.Scene();
             var width = $( this.el ).width();
             this.d_width = width;
@@ -2097,7 +2100,9 @@ module.exports = View.extend({
         scene.add(mesh);
 
         this.mesh = mesh;
-        this.camera.position.z = this.mesh.geometry.boundingSphere.radius * 2;
+
+        if(firstRun)
+            this.camera.position.z = this.mesh.geometry.boundingSphere.radius * 2;
         
         delete loader;
         delete material;            
