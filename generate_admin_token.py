a193932d807a8fdfb34a13b66ab7f741e7228c04
ahellander <andreas.hellander@it.uu.se>
2016-03-10 18:07:43 +0000

    final fix

--- generate_admin_token.py
+++ generate_admin_token.py
@@ -13,28 +13,28 @@ def print_usage_and_exit():
 def main(key):
     '''
     '''
-    url = 'http://localhost:8080/secret_key'
-    values = { 'key_string': key }
-    data = urllib.urlencode(values)
-    cnt=0;cnt_max=120
-    while cnt<cnt_max:
-        cnt+=1
-        try:
-            request = urllib2.Request(url, data)
-            response = urllib2.urlopen(request)
-            r = response.read()
-            if 'Successful secret key creation!' not in  r:
-                print response.code()
-                print response.info()
-                print r
-                raise Exception('try again')
-            break
-        except Exception as e:
-            print e
-            sys.stdout.flush()
-            time.sleep(1)
-    if cnt >= cnt_max:
-        raise Exception('Could not set admin token.')
+ #   url = 'http://localhost:8080/secret_key'
+ #   values = { 'key_string': key }
+ #   data = urllib.urlencode(values)
+ #   cnt=0;cnt_max=120
+ #   while cnt<cnt_max:
+ #       cnt+=1
+ #       try:
+ #           request = urllib2.Request(url, data)
+ #           response = urllib2.urlopen(request)
+ #           r = response.read()
+ #           if 'Successful secret key creation!' not in  r:
+ #               print response.code()
+ #               print response.info()
+ #               print r
+ #               raise Exception('try again')
+ #           break
+ #       except Exception as e:
+ #           print e
+ #           sys.stdout.flush()
+ #           time.sleep(1)
+ #   if cnt >= cnt_max:
+ #      raise Exception('Could not set admin token.')
 
     try:
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app/handlers/admin_uuid.txt'), 'w') as file:
