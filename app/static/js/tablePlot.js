80309082247c2ee8c4d12ad6e2305dee67e567b2
Ben Bales <bbbales2@gmail.com>
2016-08-28 13:24:17 -0700

    Fix for https://github.com/StochSS/stochss/issues/533

--- app/static/js/tablePlot.js
+++ app/static/js/tablePlot.js
@@ -229,7 +229,6 @@ TablePlot.TablePlot = Backbone.View.extend(
                 var chart = nv.models.lineChart()
                     .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
                     .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                    .transitionDuration(0)  //how fast do you want the lines to transition?
                     .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
                     .showYAxis(true)        //Show the y-axis
                     .showXAxis(true)//Show the x-axis
