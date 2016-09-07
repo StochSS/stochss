3426c4330bd4822434038f5b7f0b65ea1cef2b7d
Brian Drawert <bdrawert@cs.ucsb.edu>
2016-09-06 20:46:14 -0700

    updating osx launcher

--- stochss-launcher/OSX/pullImage1.7.sh
+++
@@ -1,4 +0,0 @@
-export PATH=$1:$PATH
-docker-machine env stochss1-7 >> .pullImageLog || (echo "cannot set environment" && exit -1)
-eval "$(docker-machine env stochss1-7)"
-docker images | grep "stochss/stochss-launcher" | grep -oh "1.7" || docker pull stochss/stochss-launcher:1.7
