13fd1186a0b573f9c4e4b2ebb2540b315fca6e20
Brian Drawert <bdrawert@cs.ucsb.edu>
2016-09-08 15:11:53 -0700

    stochss launcher for linux

--- stochss-launcher/Linux/stochss.sh
+++ stochss-launcher/Linux/stochss.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 trap clean_up INT SIGHUP SIGINT SIGTERM
 
+STOCHSS_IMAGE_NAME="briandrawert/stochss-launcher"
+STOCHSS_CONTAINER_NAME="stochsscontainer1_8"
+STOCHSS_VM_NAME="stochss1-8"
+STOCHSS_IMAGE_TAG="1.8"
+STOCHSS_VERSION="1.8"
+
 function clean_up(){
 	echo
-	echo "Please wait while StochSS 1.7 is stopped correctly..."
+	echo "Please wait while StochSS $STOCHSS_VERSION is stopped correctly..."
 	IR="$( cd ~/.stochss; pwd )"
-	sudo docker stop stochsscontainer1_7 >> $IR/.dockerlog 2>&1
+	docker stop $STOCHSS_CONTAINER_NAME >> $IR/.dockerlog 2>&1
 	echo "Done"
 	exit 0
 }
 
-if [[ $(uname -s) == 'Linux' ]]
+if [[ $(uname -s) == 'Darwin' ]]
 then
 	if [ ! -d ~/.stochss ]; then mkdir ~/.stochss; fi
 	DIR="$( cd ~/.stochss; pwd )"
 	(cat $DIR/.admin_key >> $DIR/.dockerlog 2>&1) || (touch $DIR/.admin_key && echo `uuidgen` > $DIR/.admin_key && echo "Generated key.")
 	token=`cat $DIR/.admin_key`
-	sudo docker start stochsscontainer1_7 >> $DIR/.dockerlog 2>&1 || { sudo docker run -d -p 8080:8080 -p 8000:8000 --name=stochsscontainer1_7 stochss/stochss-launcher:1.7 sh -c "cd stochss-master; ./run.ubuntu.sh -t $token --yy" >> $DIR/.dockerlog && echo "Starting StochSS 1.7 for the first time." && echo "To view Logs, run \"sudo docker logs -f stochsscontainer1_7\" from another terminal"; } ||	{ echo "Failed to start server."; clean_up; exit; }
+	docker start $STOCHSS_CONTAINER_NAME >> $DIR/.dockerlog 2>&1 || { docker run -d -p 8080:8080 -p 9999:9999 --name=$STOCHSS_CONTAINER_NAME $STOCHSS_IMAGE_NAME:$STOCHSS_IMAGE_TAG sh -c "cd stochss-master; ./run.ubuntu.sh -t $token --yy -a 0.0.0.0" >> $DIR/.dockerlog && echo "Starting StochSS $STOCHSS_VERSION for the first time." && echo "To view Logs, run \"docker logs -f $STOCHSS_CONTAINER_NAME\" from another terminal"; } ||	{ echo "Failed to start server."; clean_up; exit; }
 
 
 	echo "Starting server. This process may take up to 5 minutes..."
-	until $(curl --output /dev/null --silent --head --fail $(sudo docker inspect --format {{.NetworkSettings.IPAddress}} stochsscontainer1_7):8080);
+	until $(curl --output /dev/null --silent --head --fail $(docker inspect --format {{.NetworkSettings.IPAddress}} $STOCHSS_CONTAINER_NAME):8080);
 	do
 		sleep 10
 	done
 	echo "StochSS server is running at the following URL. The browser window should open automatically."
-	echo "http://$(sudo docker inspect --format {{.NetworkSettings.IPAddress}} stochsscontainer1_7):8080/login?secret_key=$token"
-	xdg-open "http://$(sudo docker inspect --format {{.NetworkSettings.IPAddress}} stochsscontainer1_7):8080/login?secret_key=$token"
+	echo "http://$(docker inspect --format {{.NetworkSettings.IPAddress}} $STOCHSS_CONTAINER_NAME):8080/login?secret_key=$token"
+    xdg-open "http://$(sudo docker inspect --format {{.NetworkSettings.IPAddress}} stochsscontainer1_7):8080/login?secret_key=$token"
 
 else
 	echo "This operating system is not recognized."
