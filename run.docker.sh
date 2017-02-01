02565a7bbe4f59d098823b93070c0d58cafe474b
Brian Drawert <bdrawert@cs.ucsb.edu>
2017-02-01 10:39:42 -0800

    trying to get mac working

--- run.docker.sh
+++ run.docker.sh
@@ -9,12 +9,16 @@
 #sudo docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume `pwd`:/stochss-master stochss/stochss-launcher:1.7 sh -c "cd /stochss-master; ./run.ubuntu.sh --yy -a 0.0.0.0"
 #=======
 docker rm stochsscontainer1_8_dev
+IMAGE_NAME='aviralcse/stochss_qsub:works_on_mac'
+#IMAGE_NAME='stochss/stochss-launcher:1.8'
 if [[ $(uname -s) == 'Darwin' ]]
 then
     PWD=$(pwd)
-    docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume /Users:/Users briandrawert/stochss-launcher:1.8 sh -c "cd $PWD; ./run.ubuntu.sh --yy -a 0.0.0.0"
+    #docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume /Users:/Users briandrawert/stochss-launcher:1.8 sh -c "cd $PWD; ./run.ubuntu.sh --yy -a 0.0.0.0"
+    #docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume /Users:/Users $IMAGE_NAME sh -c "cd $PWD; ./run.ubuntu.sh --yy -a 0.0.0.0"
+    docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume /Users:/Users $IMAGE_NAME sh -c "cd $PWD; bash"
 else
-    docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume `pwd`:/stochss-master briandrawert/stochss-launcher:1.8 sh -c "cd /stochss-master; ./run.ubuntu.sh --yy -a 0.0.0.0"
+    docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume `pwd`:/stochss-master $IMAGE_NAME sh -c "cd /stochss-master; ./run.ubuntu.sh --yy -a 0.0.0.0"
 fi
 
 
