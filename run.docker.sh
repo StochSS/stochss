f504eb0ab2228ea1854005fa01876bc96e0a9ff5
Brian Drawert <bdrawert@cs.ucsb.edu>
2017-02-14 14:13:30 -0800

    change container

--- run.docker.sh
+++ run.docker.sh
@@ -1,5 +1,5 @@
 docker rm stochsscontainer1_8_dev
-sudo docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume `pwd`:/stochss-master briandrawert/stochss-launcher:1.8beta sh -c "cd /stochss-master; ./run.ubuntu.sh --yy -a 0.0.0.0"
+sudo docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume `pwd`:/stochss-master briandrawert/stochss-launcher:1.8 sh -c "cd /stochss-master; ./run.ubuntu.sh --yy -a 0.0.0.0"
 #sudo docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev --volume `pwd`:/stochss-master stochss/stochss-launcher:1.7 sh -c "cd /stochss-master; ./run.ubuntu.sh --yy -a 0.0.0.0"
 
 
