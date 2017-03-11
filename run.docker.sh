becd996eb8855cdfcdf26a3b80aeb3149c228f7e
Aviral Takkar <aviraltakkar@gmail.com>
2017-03-10 16:14:51 -0800

    update stochss-launcher for Linux

--- run.docker.sh
+++ run.docker.sh
@@ -3,10 +3,4 @@
 docker rm stochsscontainer1_8_dev_test
 IMAGE_NAME='aviralcse/stochss_qsub:updated_stochss_for_release'
 
-#if [[ $(uname -s) == 'Darwin' ]]
-#then
-    #PWD=$(pwd)
-    #docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev $IMAGE_NAME sh -c "cd $PWD; bash"
-#else
-docker run -it -p 8080:8080 -p 8000:8000 -p 9999:9999 --name=stochsscontainer1_8_dev_test $IMAGE_NAME bash -c "cd /stochss-master && export PYTHONPATH=/stochss-master/app/lib/:/usr/local/lib/python2.7/dist-packages:/stochss-master/sdk/python:/stochss-master/sdk/python/google:/stochss-master/sdk/python/lib && ./run.ubuntu.sh -a 0.0.0.0"
-#fi
+docker run -it -p 8080:8080 -p 8000:8000 -p 9998:9999 --name=stochsscontainer1_8_dev_test $IMAGE_NAME bash -c "cd /stochss-master && export PYTHONPATH=/stochss-master/app/lib/:/usr/local/lib/python2.7/dist-packages:/stochss-master/sdk/python:/stochss-master/sdk/python/google:/stochss-master/sdk/python/lib && ./run.ubuntu.sh -a 0.0.0.0"
--- stochss-launcher/OSX/pullImage.sh
+++ stochss-launcher/OSX/pullImage.sh
@@ -6,11 +6,11 @@ export PATH=$1:$PATH
 #STOCHSS_IMAGE_TAG="1.7"
 #STOCHSS_VERSION="1.7"
 
-STOCHSS_IMAGE_NAME="briandrawert/stochss-launcher"
-STOCHSS_CONTAINER_NAME="stochsscontainer1_8"
-STOCHSS_VM_NAME="stochss1-8"
-STOCHSS_IMAGE_TAG="1.8"
-STOCHSS_VERSION="1.8"
+STOCHSS_IMAGE_NAME="aviralcse/stochss_qsub"
+STOCHSS_CONTAINER_NAME="stochsscontainer1_9"
+STOCHSS_VM_NAME="stochss1-9"
+STOCHSS_IMAGE_TAG="updated_stochss_for_release"
+STOCHSS_VERSION="1.9"
 
 docker-machine env $STOCHSS_VM_NAME >> .pullImageLog || (echo "cannot set environment" && exit -1)
 eval "$(docker-machine env $STOCHSS_VM_NAME)"
