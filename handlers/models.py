d37069a120c5f498a6f9965cbabfad189eca209c
Bryan Rumsey <bmrumsey@gmail.com>
2019-10-01 15:56:45 -0400

    Added the rename api and script to rename files as long as the new file name is unique.

--- handlers/models.py
+++ handlers/models.py
@@ -127,3 +127,21 @@ class ModelBrowserFileList(BaseHandler):
         fcode, _fslist = container.exec_run(cmd='ls.py {0} {1}'.format(file_path, path))
         fslist = _fslist.decode()
         self.write(fslist)
+
+
+class MoveRenameAPIHandler(BaseHandler):
+
+    @web.authenticated
+    async def get(self, _path):
+        checkUserOrRaise(self)
+        client = docker.from_env()
+        user = self.current_user.name
+        container = client.containers.list(filters={'name': 'jupyter-{0}'.format(user)})[0]
+        old_path, new_name = _path.split('/<--change-->/')
+        dir_path = old_path.split('/')
+        dir_path.pop()
+        dir_path.append(new_name)
+        new_path = '/'.join(dir_path)
+        fcode, _message = container.exec_run(cmd='rename.py {0} {1}'.format(old_path, new_path))
+        message = _message.decode()
+        self.write("{0}<-_path->{1}".format(message, new_path))
