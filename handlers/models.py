6d784d75e9620da10374bc6597bf466eeaffa80b
Bryan Rumsey <bmrumsey@gmail.com>
2019-10-03 15:50:18 -0400

    Fixed a bug that was preventing files with spaces in to name from being converted.  Fixed a bug that was prevents the top level of the tree from refreshing after the convertion.

--- handlers/models.py
+++ handlers/models.py
@@ -110,7 +110,7 @@ class ModelToNotebookHandler(BaseHandler):
         user = self.current_user.name
         container = client.containers.list(filters={'name': 'jupyter-{0}'.format(user)})[0]
         file_path = '/home/jovyan{0}'.format(path)
-        fcode, _fslist = container.exec_run(cmd='convert_to_notebook.py {0}'.format(path))
+        fcode, _fslist = container.exec_run(cmd='convert_to_notebook.py "{0}"'.format(path))
         fslist = _fslist.decode()
         self.write(fslist)
 
