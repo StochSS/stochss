3fe13ac4fa3a7f8a75992d2e2ea2b0b8701e7028
seanebum <smatthe2@unca.edu>
2019-10-10 14:45:55 -0400

    created simple shell script for starting minikube dev environment on MacOS and Linux.  This Performs all instructions in the readme with the exception of creating the configuration files from templates

---
+++ minikube_startup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+minikube --kubernetes-version v1.11.1 --memory 5000 --cpus 2 --vm-driver=virtualbox start
+kubectl create -f tiller-sa.yaml
+helm init --service-account tiller --wait --history-max 200
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
+
+eval $(minikube docker-env)
+docker build -t stochss-hub:dev .
+cd singleuser
+docker build -t stochss-singleuser:dev .
+
+helm upgrade --install jhub jupyterhub/jupyterhub \
+      --namespace jhub \
+      --version 0.8.2 \
+      --values config-minikube.yaml --values config-secrets.yaml
+
+cd ..
+kubectl apply -f pods-list-sa.yaml
+
+IP=$(minikube ip)
+PORT=31212
+
+echo 'Success: You can now access stochss in your browser at:'
+echo $IP:$PORT
