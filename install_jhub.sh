ff82b79a3f9d790f37c92bb5c34b3e0903cbdba6
Matthew Geiger <mgeiger@unca.edu>
2019-11-26 14:22:28 -0500

    Add github oauth config to config.yaml and install_jhub.sh

--- install_jhub.sh
+++ install_jhub.sh
@@ -7,14 +7,26 @@
 # WARNING: The command will install jupyterhub to whatever cluster helm is configured for!
 
 source .env
+source .oauth.beta.env
 
 # This should be where the stochss repo lives on the host machine
 MOUNT_PATH=/stochss
+
+OAUTH_CONFIG="
+# GitHub authentication for production
+from oauthenticator.github import GitHubOAuthenticator
+c.JupyterHub.authenticator_class = GitHubOAuthenticator
+
+c.GitHubOAuthenticator.oauth_callback_url=\"$OAUTH_CALLBACK\"
+c.GitHubOAuthenticator.client_id=\"$CLIENT_ID\"
+c.GitHubOAuthenticator.client_secret=\"$CLIENT_SECRET\"
+"
 
 helm upgrade --install jhub jupyterhub/jupyterhub \
       --namespace jhub \
       --version 0.8.2 \
       --values config.yaml \
+      --set hub.extraConfig.oauth="$OAUTH_CONFIG" \
       --set hub.cookieSecret="$(openssl rand -hex 32)" \
       --set proxy.secretToken="$(openssl rand -hex 32)" \
       --set hub.image.name="$DOCKER_HUB_IMAGE" \
