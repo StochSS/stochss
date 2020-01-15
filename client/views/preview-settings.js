b5582060578bcfb8f9c2028164421edf4d7f8665
Bryan Rumsey <bmrumsey@gmail.com>
2020-01-15 11:05:37 -0500

    Created a preview settings container for the model editor.

---
+++ client/views/preview-settings.js
@@ -0,0 +1,77 @@
+var tests = require('./tests');
+var $ = require('jquery');
+//views
+var View = require('ampersand-view');
+var InputView = require('./input');
+//templates
+var template = require('../templates/includes/previewSettings.pug');
+
+module.exports = View.extend({
+  template: template,
+  events: {
+    'click [data-hook=collapse]' : 'changeCollapseButtonText',
+  },
+  bindings: {
+  },
+  initialize: function (attrs, options) {
+    View.prototype.initialize.apply(this, arguments);
+  },
+  render: function () {
+    View.prototype.render.apply(this, arguments);
+  },
+  update: function () {
+  },
+  updateValid: function () {
+  },
+  changeCollapseButtonText: function (e) {
+    var text = $(this.queryByHook('collapse')).text();
+    text === '+' ? $(this.queryByHook('collapse')).text('-') : $(this.queryByHook('collapse')).text('+')
+  },
+  subviews: {
+    inputSimEnd: {
+      hook: 'preview-time',
+      prepareView: function (el) {
+        return new InputView({
+          parent: this,
+          required: true,
+          name: 'end-sim',
+          label: '0 to ',
+          tests: tests.valueTests,
+          modelKey: 'endSim',
+          valueType: 'number',
+          value: this.model.endSim
+        });
+      },
+    },
+    inputTimeUnit: {
+      hook: 'time-units',
+      prepareView: function (el) {
+        return new InputView ({
+          parent: this,
+          required: true,
+          name: 'time-units',
+          label: 'store state every ',
+          tests: tests.valueTests,
+          modelKey: 'timeStep',
+          valueType: 'number',
+          value: this.model.timeStep
+        });
+      },
+    },
+    inputVolume: {
+      hook: 'volume',
+      prepareView: function (el) {
+        return new InputView ({
+          parent: this,
+          required: true,
+          name: 'volume',
+          label: 'Volume: ',
+          tests: tests.valueTests,
+          modelKey: 'volume',
+          valueType: 'number',
+          value: this.model.volume,
+        });
+      },
+    },
+  },
+});
