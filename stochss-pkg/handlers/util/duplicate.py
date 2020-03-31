36c6609de66f3e9342064b4bced6aa496386b9a2
Bryan Rumsey <bmrumsey@gmail.com>
2020-03-31 14:05:27 -0400

    Added a function to copy an existing workflow as a clean workflow regradless of its status or to just copy the model.

--- stochss-pkg/handlers/util/duplicate.py
+++ stochss-pkg/handlers/util/duplicate.py
@@ -76,7 +76,7 @@ def duplicate(file_path, is_directory=False):
     return "The model {0} has been successfully copied as {1}".format(original, copy)
 
 
-def duplicate_wkfl_as_new(path):
+def duplicate_wkfl_as_new(wkfl_path, only_model):
     '''
     Copies the target workflow as a new workflow in the same parent directory.
     Copies the model of the target workflow in the same parent directory.
@@ -86,19 +86,55 @@ def duplicate_wkfl_as_new(path):
     path : str
         Path to the target workflow.
     '''
+    import json
+    from json.decoder import JSONDecodeError
+    from datetime import datetime
+    from .rename import get_unique_file_name
     from .run_model import GillesPy2Workflow
     from .parameter_sweep import ParameterSweep
     from .run_workflow import save_new_workflow
+    from .stochss_errors import ModelNotFoundError, FileNotJSONFormatError
 
-    # TODO: Get parent directory
-    # TODO: Get base name for new workflow name (current workflow name - timestamp)
-    # TODO: Get formatted timestamp for new workflow
-    # TODO: Make new workflow path in parent directory
-    # TODO: Read workflow info file for model path and workflow type
-    # TODO: Make new model path in parent directory
-    # TODO: Get unique path for the new model path
-    # TODO: Copy workflow model into parent directory
+    user_dir = '/home/jovyan'
 
-    workflows = {"gillespy":GillesPy2Workflow,"psweep":ParameterSweep}
-    wkfl = worflows[wkfl_type](wkfl_path, mdl_path)
-    save_new_wkfl(wkfl, wkfl_type, True, False)
+    full_path = path.join(user_dir, wkfl_path)
+    # Get parent directory
+    parent_dir = path.dirname(full_path)
+    # Read workflow info file for model path and workflow type
+    try:
+        with open(path.join(full_path, 'info.json'), 'r') as info_file:
+            data = json.load(info_file)
+    except FileNotFoundError as err:
+        raise StochSSFileNotFoundError("Could not read the workflow info file: " + str(err))
+    except JSONDecodeError as err:
+        raise FileNotJSONFormatError("The workflow info file is not JSON decodable: "+str(err))
+    # Make new model path in parent directory
+    model_file = data['model'].split('/').pop()
+    model_path = path.join(parent_dir, model_file)
+    # Get unique path for the new model path
+    model_path = get_unique_file_name(model_file, parent_dir)[0]
+    # Copy workflow model into parent directory
+    try:
+        copyfile(data['model'], model_path)
+    except FileNotFoundError as err:
+        raise ModelNotFoundError("Could not read the StochSS model file: " + str(err))
+    except PermissionError as err:
+        raise StochSSPermissionsError("You do not have permission to copy this file or directory: " + str(err))
+    if only_model:
+        resp = {"message":"A copy of the model in {0} has been created".format(wkfl_path),"mdlPath":model_path}
+    else:
+        # Get base name for new workflow name (current workflow name - timestamp)
+        wkfl_base_name = '_'.join(full_path.split('/').pop().split('_')[:-2])
+        # Get formatted timestamp for new workflow
+        today = datetime.now()
+        time_stamp = today.strftime("_%m%d%Y_%H%M%S")
+        # Make new workflow path in parent directory
+        new_wkfl_path = path.join(parent_dir, ''.join([wkfl_base_name, time_stamp, ".wkfl"]))
+
+        workflows = {"gillespy":GillesPy2Workflow,"psweep":ParameterSweep}
+        wkfl = workflows[data['type']](new_wkfl_path, model_path)
+        os.mkdir(new_wkfl_path)
+        save_new_workflow(wkfl, data['type'], True, False)
+
+        resp = {"message":"A new workflow has been created from {0}".format(wkfl_path),"wkflPath":new_wkfl_path,"mdlPath":model_path}
+    return resp
