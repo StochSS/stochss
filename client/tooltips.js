31a3f204ac9012f133da3738c93520643f860f66
Bryan Rumsey <bmrumsey@gmail.com>
2020-04-28 10:25:16 -0400

    Updated tooltips content based on PR comments.  Added tooltips for ensemble simulation and parameter sweep workflow selection options on the workflow selection page.  Adjusted padding for nav-link class.  Removed tooltip from the Reactions container heading and added it to the notes within the container.

--- client/tooltips.js
+++ client/tooltips.js
@@ -8,16 +8,15 @@ module.exports = {
 
     remove: "A species may only be removed if it is not a part of any reaction, event assignment, or rule.",
     
-    speciesMode: "Concentration - Species will only be represented as deterministic.<br>Population - Species will only be represented as stochastic.<br>Hybrid "+
-        "Concentration/Population - Allows a species to be represented as either deterministic or stochastic. This allow you to customize the mode of individual species "+
-        "and set the switching tolerance or minimum value for switching.",
+    speciesMode: "Concentration - Species will only be represented using continuous (floating point) values.<br>Population - Species will only be represented "+
+        "using discrete (integer count) values.<br>Hybrid Concentration/Population - Allows a species to be represented using continuous and/or discrete values.",
 
-    mode: "Concentration - Species will only be represented as deterministic.<br>Population - Species will only be represented as stochastic.<br>Hybrid "+
-        "Concentration/Population - Allows a species to be represented deterministically and/or stochastically.",
+    mode: "Concentration - Species will only be represented using continuous (floating point) values.<br>Population - Species will only be represented "+
+        "using discrete (integer count) values.<br>Hybrid Concentration/Population - Allows a species to be represented using continuous and/or discrete values.",
 
-    switchValue: "Switching Tolerance - Tolerance level for considering a dynamic species deterministically, value is compared to an estimated sd/mean population of a "+
-        "species after a given time step. This value will be used if a switch_min is not provided.<br>Minimum Value For Switching - Minimum population value at which "+
-        "species will be represented as Concentration."
+    switchValue: "Switching Tolerance - Tolerance level for considering a dynamic species deterministically, value is compared to an estimated sd/mean population "+
+        "of a species after a given time step. This value will be used if a 'Minimum Value' not provided.<br>Minimum Value For Switching - Minimum population value "+
+        "at which species will be represented as Concentration."
   },
   parametersEditor: {
     name: "Names for species, parameters, reactions, events, and rules must be unique.",
@@ -40,9 +39,6 @@ module.exports = {
     reactant: "The reactants that are consumed in the reaction, with stoichiometry.",
     
     product: "The species that are created by the reaction event, with stoichiometry.",
-    
-    reaction: "For a species that is NOT consumed in the reaction but is part of a massaction reaction, add it as both a reactant and a product.\nMass-action reactions "+
-        "must also have a rate term added. Note that the inputrate represents the mass-action constant rate independent of volume."
   },
   eventsEditor: {
     name: "Names for species, parameters, reactions, events, and rules must be unique.",
@@ -55,7 +51,8 @@ module.exports = {
     
     delay: "contains math expression evaluable within model namespace. This expression designates a delay between the trigger of an event and the execution of its assignments.",
     
-    priority: "Contains a math expression evaluable within model namespace.  This expression designates execution order for events which are executed simultaneously.",
+    priority: "Typically a number: 1, 2, etc. that designates the execution order for events which are executed simultaneously.  Can be a math expression evaluable "+
+        "within the model namespace.",
     
     initialValue: "If true, the trigger expression will be evaluated as 'True' at start of simulation.  This can be useful for some models, since an event is only "+
         "executed when the trigger expression state changes from 'False' to 'True'.",
@@ -114,19 +111,19 @@ module.exports = {
 
     seed: "The seed for the simulation.  Set to -1 for a random seed.",
 
-    realizations: "The number of times to sample the chemical master equation. Each trajectory will be returned at the end of the simulation.",
+    realizations: "The number of simulation realizations to be executed. A time-series trajectory for each realization will be returned upon completion of all "+
+        "realizations.",
 
     algorithm:"The solver by which to simulate the model.",
 
-    ode: "This algorithm produces a deterministic continuous solution via ODE.",
+    ode: "This algorithm produces a 'continuous-deterministic' solution in the style of Ordinary Differential Equations (ODE).",
 
-    ssa: "",
+    ssa: "This algorithm produces a 'discrete-stochastic' solution using the Stochastic Simulation Algorithm (SSA, also know as the Gillespie algorithm).",
 
-    tauLeaping: "This algorithm calculates multiple reactions in a single step over a given tau step size.  The change in propensities over this step are bounded "+
-        "by bounding the relative change in state, yielding greatly improved run-time performance with very little trade-off in accuracy",
+    tauLeaping: "This algorithm produces a 'discrete-stochastic' solution using a variant of the SSA that is more efficient, but less accurate.",
 
-    hybrid: "This algorithm uses a root-finding interpretation of the direct SSA method, along with ODE solvers to simulate ODE and Stochastic systems interchangeably "+
-        "or simultaneously.",
+    hybrid: "This hybrid deterministic-stochastic will produce a solution that is 'discrete-stochastic' for small population sizes and 'continuous-deterministic' "+
+        "for large population sizes.",
 
     chooseForMe: "Hybrid will be chosen based on Event, Rules, and other SBML model components or if the model is represented as Concentration.  Tau Leaping will be "+
         "chosen if the Tau Tolerance is changed to a value other than 0.03.  SSA will be chosen if the model is represented as Population."
@@ -149,8 +146,13 @@ module.exports = {
   parameterSweepResults: {
     species: "The species to view sweep data for",
 
-    mapper: "The feature of the species population/concentration data for each sweep value.",
+    mapper: "The 'feature extractor' which calculates a single value from the species population/concentration data of each simulated trajectory.",
     
-    reducer: "The type of aggregator to be applied to the trajectories of the species feature data for each sweep value."
+    reducer: "The statistical function applied to the ensemble of values extracted by the 'feature extractor' from each simulation trajectory."
   },
+  workflowSelection: {
+    ensembleSimulation: "Produce a time-series result for one or more simulation(s) of the model.",
+
+    parameterSweep: "Produced and compare results from model simulations by varying parameters over a range."
+  }
 }
