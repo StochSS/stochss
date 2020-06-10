dc875930035e56bfcc358d17ea52acdd0dfedfa4
Jesse Reeve <enszyme@gmail.com>
2020-06-09 21:44:34 -0400

    Added unit tests for all methods but duplicate_wkfl_as_new

--- stochss/tests/test_duplicate.py
+++ stochss/tests/test_duplicate.py
@@ -5,6 +5,7 @@ from handlers.util.duplicate import *
 class TestDuplicate(unittest.TestCase):
 
     #unit tests for method get_unique_file_name
+
     def test_get_unique_file_name_notcopy_noext(self):
         with tempfile.TemporaryDirectory() as tempdir:
             test_filename = "test_file"
@@ -38,13 +39,16 @@ class TestDuplicate(unittest.TestCase):
             assert test_filepath == os.path.join(tempdir,"test_file-copy(2)")
 
     #unit tests for method duplicate
+
     def test_duplicate_file_not_found_raise_error(self):
+        from handlers.util.stochss_errors import StochSSFileNotFoundError
         with tempfile.TemporaryDirectory() as tempdir:
             test_filepath = os.path.join(tempdir,"nonexistent_file")
             with self.assertRaises(StochSSFileNotFoundError):
                 duplicate(test_filepath)
 
     def test_duplicate_file_not_found_no_new_file(self):
+        from handlers.util.stochss_errors import StochSSFileNotFoundError
         with tempfile.TemporaryDirectory() as tempdir:
             test_filepath = os.path.join(tempdir,"nonexistent_file")
             try:
@@ -55,6 +59,7 @@ class TestDuplicate(unittest.TestCase):
             assert len(tempdir_contents) == 0
 
     def test_duplicate_permission_not_granted_raise_error(self):
+        from handlers.util.stochss_errors import StochSSPermissionsError
         with tempfile.TemporaryDirectory() as tempdir:
             test_filepath = os.path.join(tempdir,"existent_file")
             Path(test_filepath).touch()
@@ -63,6 +68,7 @@ class TestDuplicate(unittest.TestCase):
                 duplicate(test_filepath)
 
     def test_duplicate_permission_not_granted_no_new_file(self):
+        from handlers.util.stochss_errors import StochSSPermissionsError
         with tempfile.TemporaryDirectory() as tempdir:
             test_filepath = os.path.join(tempdir,"existent_file")
             Path(test_filepath).touch()
@@ -84,6 +90,7 @@ class TestDuplicate(unittest.TestCase):
             assert os.path.isfile(os.path.join(tempdir,'existent_file-copy'))
 
     #unit tests for method extract_wkfl_model
+
     def test_extract_wkfl_model_path_changed(self): 
         with tempfile.TemporaryDirectory() as tempdir:
             test_model_path=os.path.join(tempdir,"test_model")
@@ -109,6 +116,7 @@ class TestDuplicate(unittest.TestCase):
             assert os.path.isfile(os.path.join(tempdir,test_extract_target))
     
     def test_extract_wkfl_model_file_not_found_raise_error(self):
+        from handlers.util.stochss_errors import ModelNotFoundError
         with tempfile.TemporaryDirectory() as tempdir:
             test_model_path=os.path.join(tempdir,"test_model")
             class Test_Workflow:
@@ -129,6 +137,7 @@ class TestDuplicate(unittest.TestCase):
             assert len(tempdir_contents) == 0
     
     def test_extract_wkfl_model_permission_not_granted_raise_error(self):
+        from handlers.util.stochss_errors import StochSSPermissionsError
         with tempfile.TemporaryDirectory() as tempdir:
             test_model_path=os.path.join(tempdir,"test_model")
             Path(test_model_path).touch()
@@ -141,6 +150,7 @@ class TestDuplicate(unittest.TestCase):
                 extract_wkfl_model(wkfl=test_wkfl,mdl_parent_path=tempdir,model_file="test_model")
 
     def test_extract_wkfl_model_permission_not_granted_no_new_file(self):
+        from handlers.util.stochss_errors import StochSSPermissionsError 
         with tempfile.TemporaryDirectory() as tempdir:
             test_model_path=os.path.join(tempdir,"test_model")
             Path(test_model_path).touch()
@@ -155,3 +165,108 @@ class TestDuplicate(unittest.TestCase):
                 pass
             tempdir_contents = os.listdir(tempdir)
             assert len(tempdir_contents) == 1
+
+    #unit tests for method get_wkfl_model_parent_path
+    
+    def test_get_wkfl_model_parent_path_model_only(self):
+        assert get_wkfl_model_parent_path("test_path", True, "no wkfl") == "test_path"
+
+    def test_get_wkfl_model_parent_path_does_not_exist(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_model_path=os.path.join(tempdir,"test_model") 
+            class Test_Workflow:
+                wkfl_mdl_path = ""
+                mdl_path = ""
+            test_wkfl= Test_Workflow()
+            setattr(test_wkfl, "",test_model_path)
+            assert get_wkfl_model_parent_path("path_does_not_exist", False, test_wkfl) == "path_does_not_exist"
+
+    def test_get_wkfl_model_parent_path_exists(self): 
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_dir_path=os.path.join(tempdir,'test_directory')
+            os.mkdir(test_dir_path)
+            test_file_path = os.path.join(test_dir_path,"test_file")
+            class Test_Workflow:
+                mdl_path = ""
+            test_wkfl= Test_Workflow()
+            setattr(test_wkfl, "mdl_path", test_file_path)
+            Path(test_file_path).touch()
+            assert get_wkfl_model_parent_path("path_does_not_exist", False, test_wkfl) == test_dir_path
+
+    #unit tests for method get_model_path
+
+    def test_get_model_path_only_model(self):
+        assert get_model_path("test_wkfl_parent_path", "test_mdl_parent_path", "test_mdl_file", True) == (os.path.join("test_wkfl_parent_path","test_mdl_file"), "")
+
+    def test_get_model_path_wkfl_parent_path_equals_mdl_parent_path_and_exists(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_dual_parent_dir = os.path.join(tempdir, "test_dual_parent_dir")
+            test_model_path = os.path.join(test_dual_parent_dir,"test_model")
+            os.mkdir(test_dual_parent_dir)
+            Path(test_model_path).touch()
+            assert get_model_path(test_dual_parent_dir, test_dual_parent_dir, "test_model", False) == (os.path.join(test_dual_parent_dir,"test_model"), "")
+
+    def test_get_model_path_mdl_file_in_wkfl_parent_path_directory(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_wkfl_parent_dir = os.path.join(tempdir, "test_wkfl_parent_dir")
+            os.mkdir(test_wkfl_parent_dir)
+            test_model_parent_dir = os.path.join(tempdir, "test_model_parent_dir")
+            os.mkdir(test_model_parent_dir)
+            test_model_path = os.path.join(test_wkfl_parent_dir,"test_model")
+            Path(test_model_path).touch()
+            assert get_model_path(test_wkfl_parent_dir, "", "test_model", False) == (os.path.join(test_wkfl_parent_dir,"test_model"), "")
+
+
+    def test_get_model_path_mdl_file_in_mdl_parent_path_directory(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_wkfl_parent_dir = os.path.join(tempdir, "test_wkfl_parent_dir")
+            os.mkdir(test_wkfl_parent_dir)
+            test_model_parent_dir = os.path.join(tempdir, "test_model_parent_dir")
+            os.mkdir(test_model_parent_dir)
+            test_model_path = os.path.join(test_model_parent_dir,"test_model")
+            Path(test_model_path).touch()
+            assert get_model_path(test_wkfl_parent_dir, test_model_parent_dir, "test_model", False) == (os.path.join(test_model_parent_dir,"test_model"), "")
+
+
+    def test_get_model_path_mdl_file_in_mdl_and_wkfl_parent_path_directory(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_wkfl_parent_dir = os.path.join(tempdir, "test_wkfl_parent_dir")
+            os.mkdir(test_wkfl_parent_dir)
+            test_model_parent_dir = os.path.join(tempdir, "test_model_parent_dir")
+            os.mkdir(test_model_parent_dir)
+            test_model_path = os.path.join(test_model_parent_dir,"test_model")
+            Path(test_model_path).touch()
+            test_model_path2 = os.path.join(test_wkfl_parent_dir,"test_model")
+            Path(test_model_path2).touch()
+            assert get_model_path(test_wkfl_parent_dir, test_model_parent_dir, "test_model", False) == (os.path.join(test_wkfl_parent_dir,"test_model"), "")
+
+
+    def test_get_model_path_mdl_file_not_found(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            test_wkfl_parent_dir = os.path.join(tempdir, "test_wkfl_parent_dir")
+            os.mkdir(test_wkfl_parent_dir)
+            test_model_parent_dir = os.path.join(tempdir, "test_model_parent_dir")
+            os.mkdir(test_model_parent_dir)
+            assert get_model_path(test_wkfl_parent_dir, test_model_parent_dir, "test_model", False) == (os.path.join(test_wkfl_parent_dir,"test_model"), "The model file {0} could not be found.  To edit the model or run the workflow you will need to update the path to the model or extract the model from the workflow.".format("test_model"))
+
+
+    #unit tests for method duplicate_wkfl_as_new
+
+    def test_duplicate_wkfl_as_new_file_not_found(self):
+        from handlers.util.stochss_errors import StochSSFileNotFoundError 
+        with tempfile.TemporaryDirectory() as tempdir:
+            with self.assertRaises(StochSSFileNotFoundError):
+                duplicate_wkfl_as_new(os.path.join(tempdir,"nonexistent_wkfl"), True, "timestamp")
+
+    def test_duplicate_wkfl_as_new_not_JSON_decodable(self):
+        from handlers.util.stochss_errors import FileNotJSONFormatError
+        with tempfile.TemporaryDirectory() as tempdir:
+            Path(os.path.join(tempdir,"info.json")).touch()
+            with self.assertRaises(FileNotJSONFormatError):
+                duplicate_wkfl_as_new(os.path.join(tempdir), True, "timestamp")
+        
+
+    def test_duplicate_wkfl_as_new_only_model(self):
+        
+
+
