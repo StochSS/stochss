6fc1ff220777c49ba739ee514a29b9a99d22d7cf
Bryan Rumsey <bmrumsey@gmail.com>
2020-06-30 16:11:31 -0400

    Added function to open workflows in the workflow manager page.

--- client/views/edit-workflow-view.js
+++ client/views/edit-workflow-view.js
@@ -1,3 +1,6 @@
+var path = require('path');
+//support files
+var app = require('../app');
 //views
 var View = require('ampersand-view');
 //templates
@@ -5,11 +8,17 @@ var template = require('../templates/includes/editWorkflowView.pug');
 
 module.exports = View.extend({
   template: template,
-  events: {},
+  events: {
+    'click [data-hook=project-workflow-open]' : 'handleOpenWorkflowClick'
+  },
   initialize: function (attrs, options) {
     View.prototype.initialize.apply(this, arguments);
   },
   render: function (attrs, options) {
     View.prototype.render.apply(this, arguments);
+  },
+  handleOpenWorkflowClick: function (e) {
+    let endpoint = path.join(app.getBasePath(), "stochss/workflow/edit")+"?path="+this.model.path+"&type=none";
+    window.location.href = endpoint
   }
 });
