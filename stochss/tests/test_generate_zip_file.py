095a3811289b566f2a59605162d784e9ed484fda
Jesse Reeve <enszyme@gmail.com>
2020-07-10 13:26:47 -0400

    Added unit tests for generate_zip_file.py

--- stochss/tests/test_generate_zip_file.py
+++ stochss/tests/test_generate_zip_file.py
@@ -2,26 +2,29 @@ import os
 import unittest
 from pathlib import Path
 import handlers.util.generate_zip_file as generate_zip_file
+import handlers.util.stochss_errors as stochss_errors
 
 class TestGenerate_Zip_File(unittest.TestCase):
 
     #unit tests for method generate_zip_file
 
     def test_generate_zip_file(self):
-        from unittest import mock
         test_file = "test_file.foo"
         test_path = os.path.join("test_path",test_file)
         test_dir = "test_dir"
         test_target = "test_target"
-        with mock.patch("shutil.make_archive") as mock_make_archive:
+        with unittest.mock.patch("shutil.make_archive") as mock_make_archive:
             generate_zip_file.generate_zip_file(test_path,test_dir,test_target)
         correct_filepath = os.path.join(test_dir,"test_file")
         mock_make_archive.assert_called_with(correct_filepath,'zip',test_dir, test_target)
 
+    #unit tests for method get_zip_file_data
+
     def test_get_zip_file_data(self):
-        from unittest import mock
-        with mock.patch("builtins.open",mock.mock_open(read_data="foo")) as mock_open:
+        with unittest.mock.patch("builtins.open",unittest.mock.mock_open(read_data="foo")) as mock_open:
             assert generate_zip_file.get_zip_file_data("placeholder") == "foo"
+
+    #unit tests for method get_results_csv_dir
 
     def test_get_results_csv_dir_not_found(self):
         test_file = "results_csv"
@@ -30,9 +33,36 @@ class TestGenerate_Zip_File(unittest.TestCase):
 
     def test_get_results_csv_dir_is_found(self):
         import tempfile
-        test_file = "results_csv"
         with tempfile.TemporaryDirectory() as tempdir:
-            test_path = os.path.join(tempdir,test_file)
-            Path(test_path).touch()
-            print("Results: ",generate_zip_file.get_results_csv_dir(test_file, test_path))
+            test_dir = os.path.join(tempdir, "results_csv")
+            os.mkdir(test_dir)
+            assert generate_zip_file.get_results_csv_dir(test_dir, tempdir) == test_dir
 
+    #unit tests for method download_zip
+
+    def test_download_zip_file_invalid_path(self):
+        with self.assertRaises(stochss_errors.StochSSFileNotFoundError):
+            generate_zip_file.download_zip("placeholder_path","placeholder_action")
+
+    def test_download_zip_file_calls_generate_zip_file(self):
+        test_path = "test_dir/test_path.foo"
+        test_action = "generate"
+        with unittest.mock.patch("os.path.exists"):
+            with unittest.mock.patch("handlers.util.generate_zip_file.get_unique_file_name",\
+                    return_value="test_path"):
+                with unittest.mock.patch("handlers.util.generate_zip_file.generate_zip_file") as mock_generate:
+                    generate_zip_file.download_zip(test_path, test_action)
+        mock_generate.assert_called_with("t", os.path.join("/home/jovyan","test_dir"), \
+                os.path.join("/home/jovyan", test_path))
+
+    def test_download_zip_file_action_generate(self):
+        test_path = "test_dir/test_path.foo"
+        test_action = "generate"
+        test_resp = {"Message":"Successfully created t","Path":"t",}
+        with unittest.mock.patch("os.path.exists"):
+            with unittest.mock.patch("handlers.util.generate_zip_file.get_unique_file_name",\
+                    return_value="test_path"):
+                with unittest.mock.patch("handlers.util.generate_zip_file.generate_zip_file") as mock_generate:
+                    assert generate_zip_file.download_zip(test_path, test_action) == test_resp
+
+    #download_zip action resultscsv is not covered pending refactor of user dir from /home/jovyan
