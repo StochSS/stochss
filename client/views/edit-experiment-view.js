cd9793c4e75e7ff49ff6cb1de00dfe894149c2e2
Bryan Rumsey <bmrumsey@gmail.com>
2020-07-30 13:27:33 -0400

    Restructured the layout for the project managers experiments section. Moved the add workflow functionality from the edit-experiment view to the edit-experiments view. Moved the export as combine functionality to the browse files section. Added functionality to download the archive.

--- client/views/edit-experiment-view.js
+++ client/views/edit-experiment-view.js
@@ -14,9 +14,6 @@ module.exports = View.extend({
   template: template,
   events: {
     'click [data-hook=project-experiment-view]' : 'handleViewWorkflowsClick',
-    'click [data-hook=project-experiment-new-workflow]' : 'handleNewWorkflowClick',
-    'click [data-hook=project-experiment-add-workflow]' : 'handleAddWorkflowClick',
-    'click [data-hook=export-experiment-as-combine]' : 'handleExportCombineClick',
     'click [data-hook=project-experiment-remove]' : 'handleRemoveExperimentClick'
   },
   initialize: function (attrs, options) {
@@ -40,73 +37,11 @@ module.exports = View.extend({
     this.renderSubview(view, this.queryByHook(hook));
   },
   handleViewWorkflowsClick: function (e) {
-    let options = {"View":"show", "Hide":"hide"}
-    let newText = {"View":"Hide", "Hide":"View"}
+    let options = {"View Workflows":"show", "Hide Workflows":"hide"}
+    let newText = {"View Workflows":"Hide Workflows", "Hide Workflows":"View Workflows"}
     let text = $(this.queryByHook("project-experiment-view")).text()
     $(this.queryByHook("project-workflows-container")).collapse(options[text])
     $(this.queryByHook("project-experiment-view")).text(newText[text])
-  },
-  handleNewWorkflowClick: function (e) {
-    let self = this
-    if(document.querySelector("#newProjectWorkflowModal")) {
-      document.querySelector("#newProjectWorkflowModal").remove()
-    }
-    let modal = $(modals.newProjectWorkflowHtml("Name of the model: ")).modal()
-    let okBtn = document.querySelector("#newProjectWorkflowModal .ok-model-btn")
-    let input = document.querySelector("#newProjectWorkflowModal #input")
-    input.addEventListener("keyup", function (event) {
-      if(event.keyCode === 13){
-        event.preventDefault();
-        okBtn.click();
-      }
-    });
-    okBtn.addEventListener('click', function (e) {
-      if(Boolean(input.value)) {
-        let mdlFile = input.value.endsWith('.mdl') ? input.value : input.value + ".mdl"
-        let mdlPath =  path.join(self.parent.parent.projectPath, mdlFile)
-        let parentPath = path.join(self.parent.parent.projectPath, self.model.name)+".exp"
-        let queryString = "?path="+mdlPath+"&parentPath="+parentPath
-        let endpoint = path.join(app.getBasePath(), 'stochss/workflow/selection')+queryString
-        modal.modal('hide')
-        window.location.href = endpoint
-      }
-    });
-  },
-  handleAddWorkflowClick: function (e) {
-    let self = this
-    if(document.querySelector("#newProjectWorkflowModal")) {
-      document.querySelector("#newProjectWorkflowModal").remove()
-    }
-    let modal = $(modals.addExistingWorkflowToProjectHtml()).modal()
-    let okBtn = document.querySelector("#newProjectWorkflowModal .ok-model-btn")
-    let input = document.querySelector("#newProjectWorkflowModal #workflowPathInput")
-    input.addEventListener("keyup", function (event) {
-      if(event.keyCode === 13){
-        event.preventDefault();
-        okBtn.click();
-      }
-    });
-    okBtn.addEventListener('click', function (e) {
-      if(Boolean(input.value)) {
-        let expPath = path.join(self.parent.parent.projectPath, self.model.name)+".exp"
-        let queryString = "?path="+expPath+"&wkflPath="+input.value
-        let endpoint = path.join(app.getApiPath(), "project/add-existing-workflow")+queryString
-        xhr({uri: endpoint, json: true}, function (err, response, body) {
-          if(response.statusCode < 400) {
-            self.parent.parent.update("experiment-editor")
-            let successModal = $(modals.addExistingWorkflowToProjectSuccessHtml(body.message)).modal()
-          }else{
-            let errorModal = $(modals.addExistingWorkflowToProjectErrorHtml(body.Reason, body.Message)).modal()
-          }
-        });
-        modal.modal('hide')
-      }
-    });
-  },
-  handleExportCombineClick: function (e) {
-    let parentPath = this.parent.parent.projectPath
-    let target = path.join(parentPath, this.model.name + ".exp")
-    this.exportAsCombine(target)
   },
   handleRemoveExperimentClick: function (e) {
     let self = this
@@ -128,7 +63,4 @@ module.exports = View.extend({
       modal.modal('hide')
     });
   },
-  exportAsCombine: function (target) {
-    this.parent.parent.exportAsCombine(target)
-  }
 });
