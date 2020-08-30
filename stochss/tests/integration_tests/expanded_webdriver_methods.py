175b9416094d4c62e11d59feeeed4a7d2d41dc64
Jesse Reeve <enszyme@gmail.com>
2020-08-29 21:34:39 -0400

    Filesystem and model editor tests complete

--- stochss/tests/integration_tests/expanded_webdriver_methods.py
+++ stochss/tests/integration_tests/expanded_webdriver_methods.py
@@ -20,38 +20,6 @@ def element_has_gone_stale(self, element):
     except selenium.common.exceptions.StaleElementReferenceException:
         return True
 
-def find_by_text(passed_list, key):
-    for item in passed_list:
-        if (item.text==key):
-            return item
-    return None
-
-def click_element_by_id(self, key):
-    element=self.find_element_by_id(key)
-    if (element):
-        element.click()
-
-def click_element_by_class_and_text(self, class_name, target_element_text):
-    element_list=self.find_elements_by_class_name(class_name)
-    target_element=find_by_text(element_list, target_element_text)
-    if (target_element):
-        target_element.click()
-
-def enter_modal_text(self, text):
-    text_box=self.find_element_by_id("modelNameInput")
-    text_box.send_keys(text)
-    self.click_element_by_class_and_text("ok-model-btn", "OK")
-
-def new_file_directory_new_directory(self, dir_name):
-    self.click_element_by_id("new-file-directory")
-    self.click_element_by_id("new-directory")
-    self.enter_modal_text(dir_name)
-
-def new_file_directory_new_model(self, model_name):
-    self.click_element_by_id("new-file-directory")
-    self.click_element_by_id("new-model")
-    self.enter_modal_text(model_name)
-
 def wait_for_navigation_complete(self):
     wait_for(function=self.element_has_gone_stale, element=self.find_element_by_id('navbar'))
     time.sleep(3)
