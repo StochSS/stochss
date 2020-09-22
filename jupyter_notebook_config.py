3798e700e34251355bc618d30b39e001df131451
Bryan Rumsey <bmrumsey@gmail.com>
2020-09-22 10:31:14 -0400

    Added modal to inform the user if an uploading zip file already exists.

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -47,7 +47,7 @@ c.NotebookApp.nbserver_extensions = {
 }
 
 ## Extra paths to search for serving static files.
-#  
+#
 #  This allows adding javascript/css to be available from the notebook server
 #  machine, or overriding individual files in the IPython
 #
@@ -58,13 +58,13 @@ c.NotebookApp.extra_static_paths = [
 ]
 
 ## Disable cross-site-request-forgery protection
-#  
+#
 #  Jupyter notebook 4.3.1 introduces protection from cross-site request
 #  forgeries, requiring API requests to either:
-#  
+#
 #  - originate from pages served by this server (validated with XSRF cookie and
 #  token), or - authenticate with a token
-#  
+#
 #  Some anonymous compute resources still desire the ability to run code,
 #  completely without authentication. These services can disable all
 #  authentication and security checks, with the full knowledge of what that
@@ -75,7 +75,7 @@ c.NotebookApp.disable_check_xsrf = True
 #c.NotebookApp.notebook_dir = ''
 
 ## Extra paths to search for serving jinja templates.
-#  
+#
 #  Can be used to override templates from notebook.templates.
 #c.NotebookApp.extra_template_paths = []
 
@@ -83,42 +83,42 @@ c.NotebookApp.disable_check_xsrf = True
 #c.NotebookApp.allow_credentials = False
 
 ## Set the Access-Control-Allow-Origin header
-#  
+#
 #  Use '*' to allow any origin to access your server.
-#  
+#
 #  Takes precedence over allow_origin_pat.
 #c.NotebookApp.allow_origin = ''
 
 ## Use a regular expression for the Access-Control-Allow-Origin header
-#  
+#
 #  Requests from an origin matching the expression will get replies with:
-#  
+#
 #      Access-Control-Allow-Origin: origin
-#  
+#
 #  where `origin` is the origin of the request.
-#  
+#
 #  Ignored if allow_origin is set.
 #c.NotebookApp.allow_origin_pat = ''
 
 ## Allow password to be changed at login for the notebook server.
-#  
+#
 #  While loggin in with a token, the notebook server UI will give the opportunity
 #  to the user to enter a new password at the same time that will replace the
 #  token login mechanism.
-#  
+#
 #  This can be set to false to prevent changing password from the UI/API.
 #c.NotebookApp.allow_password_change = True
 
 ## Allow requests where the Host header doesn't point to a local server
-#  
+#
 #  By default, requests get a 403 forbidden response if the 'Host' header shows
 #  that the browser thinks it's on a non-local domain. Setting this option to
 #  True disables this check.
-#  
+#
 #  This protects against 'DNS rebinding' attacks, where a remote web server
 #  serves you a page and then changes its DNS to send later requests to a local
 #  IP, bypassing same-origin checks.
-#  
+#
 #  Local IP addresses (such as 127.0.0.1 and ::1) are allowed as local, along
 #  with hostnames configured in local_hostnames.
 #c.NotebookApp.allow_remote_access = False
@@ -130,7 +130,7 @@ c.NotebookApp.disable_check_xsrf = True
 #c.NotebookApp.base_project_url = '/'
 
 ## The base URL for the notebook server.
-#  
+#
 #  Leading and trailing slashes can be omitted, and will automatically be added.
 #c.NotebookApp.base_url = '/'
 
@@ -151,7 +151,8 @@ c.NotebookApp.disable_check_xsrf = True
 #c.NotebookApp.config_manager_class = 'notebook.services.config.manager.ConfigManager'
 
 ## The notebook manager class to use.
-#c.NotebookApp.contents_manager_class = 'notebook.services.contents.largefilemanager.LargeFileManager'
+#c.NotebookApp.contents_manager_class = 'notebook.services.contents.largefilemanager.\
+#                                          LargeFileManager'
 
 ## Extra keyword arguments to pass to `set_secure_cookie`. See tornado's
 #  set_secure_cookie docs for details.
@@ -160,7 +161,7 @@ c.NotebookApp.disable_check_xsrf = True
 ## The random bytes used to secure cookies. By default this is a new random
 #  number every time you start the Notebook. Set it to a value in a config file
 #  to enable logins to persist across server sessions.
-#  
+#
 #  Note: Cookie secrets should be kept private, do not share config files with
 #  cookie_secret stored in plaintext (you can read the value from a file).
 #c.NotebookApp.cookie_secret = b''
@@ -169,12 +170,12 @@ c.NotebookApp.disable_check_xsrf = True
 #c.NotebookApp.cookie_secret_file = ''
 
 ## Override URL shown to users.
-#  
+#
 #  Replace actual URL, including protocol, address, port and base URL, with the
 #  given value when displaying URL to the users. Do not change the actual
 #  connection URL. If authentication token is enabled, the token is added to the
 #  custom URL automatically.
-#  
+#
 #  This option is intended to be used when the URL to display to the user cannot
 #  be determined reliably by the Jupyter notebook server (proxified or
 #  containerized setups for example).
@@ -185,11 +186,11 @@ c.NotebookApp.default_url = '/stochss/home'
 
 
 ## Whether to enable MathJax for typesetting math/TeX
-#  
+#
 #  MathJax is the javascript library Jupyter uses to render math/LaTeX. It is
 #  very large, so you may want to disable it if you have a slow internet
 #  connection, or for offline use of the notebook.
-#  
+#
 #  When disabled, equations etc. will appear as their untransformed TeX source.
 #c.NotebookApp.enable_mathjax = True
 
@@ -199,7 +200,7 @@ c.NotebookApp.default_url = '/stochss/home'
 ## handlers that should be loaded at higher priority than the default services
 #c.NotebookApp.extra_services = []
 
-## 
+##
 #c.NotebookApp.file_to_run = ''
 
 ## Extra keyword arguments to pass to `get_secure_cookie`. See tornado's
@@ -232,7 +233,7 @@ c.NotebookApp.default_url = '/stochss/home'
 
 ## The kernel spec manager class to use. Should be a subclass of
 #  `jupyter_client.kernelspec.KernelSpecManager`.
-#  
+#
 #  The Api of KernelSpecManager is provisional and might change without warning
 #  between this version of Jupyter and the next stable one.
 #c.NotebookApp.kernel_spec_manager_class = 'jupyter_client.kernelspec.KernelSpecManager'
@@ -241,7 +242,7 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.NotebookApp.keyfile = ''
 
 ## Hostnames to allow as local when allow_remote_access is False.
-#  
+#
 #  Local IP addresses (such as 127.0.0.1 and ::1) are automatically accepted as
 #  local as well.
 #c.NotebookApp.local_hostnames = ['localhost']
@@ -262,7 +263,7 @@ c.NotebookApp.default_url = '/stochss/home'
 ## Sets the maximum allowed size of the client request body, specified in  the
 #  Content-Length request header field. If the size in a request  exceeds the
 #  configured value, a malformed HTTP message is returned to the client.
-#  
+#
 #  Note: max_body_size is applied even in streaming mode.
 #c.NotebookApp.max_body_size = 536870912
 
@@ -282,18 +283,18 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.NotebookApp.open_browser = True
 
 ## Hashed password to use for web authentication.
-#  
+#
 #  To generate, type in a python/IPython shell:
-#  
+#
 #    from notebook.auth import passwd; passwd()
-#  
+#
 #  The string should be of the form type:salt:hashed-password.
 #c.NotebookApp.password = ''
 
 ## Forces users to use a password for the Notebook server. This is useful in a
 #  multi user environment, for instance when everybody in the LAN can access each
 #  other's machine through ssh.
-#  
+#
 #  In such a case, server the notebook server on localhost is not secure since
 #  any user can connect to the notebook server via ssh.
 #c.NotebookApp.password_required = False
@@ -338,18 +339,18 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.NotebookApp.terminado_settings = {}
 
 ## Set to False to disable terminals.
-#  
+#
 #  This does *not* make the notebook server more secure by itself. Anything the
 #  user can in a terminal, they can also do in a notebook.
-#  
+#
 #  Terminals may also be automatically disabled if the terminado package is not
 #  available.
 #c.NotebookApp.terminals_enabled = True
 
 ## Token used for authenticating first-time connections to the server.
-#  
+#
 #  When no password is enabled, the default is to generate a new, random token.
-#  
+#
 #  Setting to an empty string disables authentication altogether, which is NOT
 #  RECOMMENDED.
 #c.NotebookApp.token = '<generated>'
@@ -364,17 +365,17 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.NotebookApp.trust_xheaders = False
 
 ## Disable launching browser by redirect file
-#  
+#
 #  For versions of notebook > 5.7.2, a security feature measure was added that
 #  prevented the authentication token used to launch the browser from being
 #  visible. This feature makes it difficult for other users on a multi-user
 #  system from running code in your Jupyter session as you.
-#  
+#
 #  However, some environments (like Windows Subsystem for Linux (WSL) and
 #  Chromebooks), launching a browser using a redirect file can lead the browser
 #  failing to load.  This is because of the difference in file structures/paths
 #  between the runtime and  the browser.
-#  
+#
 #  Disabling this setting to False will disable this behavior, allowing the
 #  browser  to launch by using a URL and visible token (as before).
 #c.NotebookApp.use_redirect_file = True
@@ -385,26 +386,26 @@ c.NotebookApp.default_url = '/stochss/home'
 ## Specify Where to open the notebook on startup. This is the `new` argument
 #  passed to the standard library method `webbrowser.open`. The behaviour is not
 #  guaranteed, but depends on browser support. Valid values are:
-#  
+#
 #   - 2 opens a new tab,
 #   - 1 opens a new window,
 #   - 0 opens in an existing window.
-#  
+#
 #  See the `webbrowser.open` documentation for details.
 #c.NotebookApp.webbrowser_open_new = 2
 
 ## Set the tornado compression options for websocket connections.
-#  
+#
 #  This value will be returned from
 #  :meth:`WebSocketHandler.get_compression_options`. None (default) will disable
 #  compression. A dict (even an empty one) will enable compression.
-#  
+#
 #  See the tornado docs for WebSocketHandler.get_compression_options for details.
 #c.NotebookApp.websocket_compression_options = None
 
 ## The base URL for websockets, if it differs from the HTTP server (hint: it
 #  almost certainly doesn't).
-#  
+#
 #  Should be in the form of an HTTP origin: ws[s]://hostname[:port]
 #c.NotebookApp.websocket_url = ''
 
@@ -453,7 +454,7 @@ c.NotebookApp.default_url = '/stochss/home'
 ## Mixin for configurable classes that work with connection files
 
 ## JSON file in which to store connection info [default: kernel-<pid>.json]
-#  
+#
 #  This file will contain the IP, ports, and authentication key needed to connect
 #  clients to this kernel. By default, this file will be created in the security
 #  dir of the current profile, but can be specified by absolute path.
@@ -479,7 +480,7 @@ c.NotebookApp.default_url = '/stochss/home'
 ## set the stdin (ROUTER) port [default: random]
 #c.ConnectionFileMixin.stdin_port = 0
 
-## 
+##
 #c.ConnectionFileMixin.transport = 'tcp'
 
 #------------------------------------------------------------------------------
@@ -487,14 +488,14 @@ c.NotebookApp.default_url = '/stochss/home'
 #------------------------------------------------------------------------------
 
 ## Manages a single kernel in a subprocess on this host.
-#  
+#
 #  This version starts kernels with Popen.
 
 ## Should we autorestart the kernel if it dies.
 #c.KernelManager.autorestart = True
 
 ## DEPRECATED: Use kernel_name instead.
-#  
+#
 #  The Popen Command to launch the kernel. Override this if you have a custom
 #  kernel. If kernel_cmd is specified in a configuration file, Jupyter does not
 #  pass any arguments to the kernel, because it cannot make any assumptions about
@@ -511,27 +512,27 @@ c.NotebookApp.default_url = '/stochss/home'
 #------------------------------------------------------------------------------
 
 ## Object for handling serialization and sending of messages.
-#  
+#
 #  The Session object handles building messages and sending them with ZMQ sockets
 #  or ZMQStream objects.  Objects can communicate with each other over the
 #  network via Session objects, and only need to work with the dict-based IPython
 #  message spec. The Session will handle serialization/deserialization, security,
 #  and metadata.
-#  
+#
 #  Sessions support configurable serialization via packer/unpacker traits, and
 #  signing with HMAC digests via the key/keyfile traits.
-#  
+#
 #  Parameters ----------
-#  
+#
 #  debug : bool
 #      whether to trigger extra debugging statements
 #  packer/unpacker : str : 'json', 'pickle' or import_string
 #      importstrings for methods to serialize message parts.  If just
 #      'json' or 'pickle', predefined JSON and pickle packers will be used.
 #      Otherwise, the entire importstring must be used.
-#  
+#
 #      The functions must accept at least valid JSON input, and output *bytes*.
-#  
+#
 #      For example, to use msgpack:
 #      packer = 'msgpack.packb', unpacker='msgpack.unpackb'
 #  pack/unpack : callables
@@ -552,7 +553,7 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.Session.buffer_threshold = 1024
 
 ## Whether to check PID to protect against calls after fork.
-#  
+#
 #  This check can be disabled if fork-safety is handled elsewhere.
 #c.Session.check_pid = True
 
@@ -563,7 +564,7 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.Session.debug = False
 
 ## The maximum number of digests to remember.
-#  
+#
 #  The digest history will be culled when it exceeds this value.
 #c.Session.digest_history_size = 65536
 
@@ -624,10 +625,10 @@ c.NotebookApp.default_url = '/stochss/home'
 
 ## Whether messages from kernels whose frontends have disconnected should be
 #  buffered in-memory.
-#  
+#
 #  When True (default), messages are buffered and replayed on reconnect, avoiding
 #  lost messages due to interrupted connectivity.
-#  
+#
 #  Disable if long-running kernels will produce too much output while no
 #  frontends are connected.
 #c.MappingKernelManager.buffer_offline_messages = True
@@ -650,7 +651,7 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.MappingKernelManager.cull_interval = 300
 
 ## Timeout for giving up on a kernel (in seconds).
-#  
+#
 #  On starting and restarting kernels, we check whether the kernel is running and
 #  responsive by sending kernel_info_requests. This sets the timeout in seconds
 #  for how long the kernel can take before being presumed dead. This affects the
@@ -658,7 +659,7 @@ c.NotebookApp.default_url = '/stochss/home'
 #  ZMQChannelsHandler (which handles the startup).
 #c.MappingKernelManager.kernel_info_timeout = 60
 
-## 
+##
 #c.MappingKernelManager.root_dir = ''
 
 #------------------------------------------------------------------------------
@@ -674,7 +675,7 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.KernelSpecManager.kernel_spec_class = 'jupyter_client.kernelspec.KernelSpec'
 
 ## Whitelist of allowed kernel names.
-#  
+#
 #  By default, all installed kernels are allowed.
 #c.KernelSpecManager.whitelist = set()
 
@@ -683,13 +684,13 @@ c.NotebookApp.default_url = '/stochss/home'
 #------------------------------------------------------------------------------
 
 ## Base class for serving files and directories.
-#  
+#
 #  This serves any text or binary file, as well as directories, with special
 #  handling for JSON notebook documents.
-#  
+#
 #  Most APIs take a path argument, which is always an API-style unicode path, and
 #  always refers to a directory.
-#  
+#
 #  - unicode, not url-escaped
 #  - '/'-separated
 #  - leading and trailing '/' will be stripped
@@ -699,53 +700,54 @@ c.NotebookApp.default_url = '/stochss/home'
 ## Allow access to hidden files
 #c.ContentsManager.allow_hidden = False
 
-## 
+##
 #c.ContentsManager.checkpoints = None
 
-## 
+##
 #c.ContentsManager.checkpoints_class = 'notebook.services.contents.checkpoints.Checkpoints'
 
-## 
+##
 #c.ContentsManager.checkpoints_kwargs = {}
 
 ## handler class to use when serving raw file requests.
-#  
+#
 #  Default is a fallback that talks to the ContentsManager API, which may be
 #  inefficient, especially for large files.
-#  
+#
 #  Local files-based ContentsManagers can use a StaticFileHandler subclass, which
 #  will be much more efficient.
-#  
+#
 #  Access to these files should be Authenticated.
 #c.ContentsManager.files_handler_class = 'notebook.files.handlers.FilesHandler'
 
 ## Extra parameters to pass to files_handler_class.
-#  
+#
 #  For example, StaticFileHandlers generally expect a `path` argument specifying
 #  the root directory from which to serve files.
 #c.ContentsManager.files_handler_params = {}
 
 ## Glob patterns to hide in file and directory listings.
-#c.ContentsManager.hide_globs = ['__pycache__', '*.pyc', '*.pyo', '.DS_Store', '*.so', '*.dylib', '*~']
+#c.ContentsManager.hide_globs = ['__pycache__', '*.pyc', '*.pyo', '.DS_Store',
+#                                '*.so', '*.dylib', '*~']
 
 ## Python callable or importstring thereof
-#  
+#
 #  To be called on a contents model prior to save.
-#  
+#
 #  This can be used to process the structure, such as removing notebook outputs
 #  or other side effects that should not be saved.
-#  
+#
 #  It will be called as (all arguments passed by keyword)::
-#  
+#
 #      hook(path=path, model=model, contents_manager=self)
-#  
+#
 #  - model: the model to be saved. Includes file contents.
 #    Modifying this dict will affect the file that is stored.
 #  - path: the API path of the save destination
 #  - contents_manager: this ContentsManager instance
 #c.ContentsManager.pre_save_hook = None
 
-## 
+##
 #c.ContentsManager.root_dir = '/'
 
 ## The base name used when creating untitled directories.
@@ -762,17 +764,17 @@ c.NotebookApp.default_url = '/stochss/home'
 #------------------------------------------------------------------------------
 
 ## Mixin for ContentsAPI classes that interact with the filesystem.
-#  
+#
 #  Provides facilities for reading, writing, and copying both notebooks and
 #  generic files.
-#  
+#
 #  Shared by FileContentsManager and FileCheckpoints.
-#  
+#
 #  Note ---- Classes using this mixin must provide the following attributes:
-#  
+#
 #  root_dir : unicode
 #      A directory against against which API-style paths are to be resolved.
-#  
+#
 #  log : logging.Logger
 
 ## By default notebooks are saved on disk on a temporary file and then if
@@ -793,21 +795,21 @@ c.NotebookApp.default_url = '/stochss/home'
 #c.FileContentsManager.delete_to_trash = True
 
 ## Python callable or importstring thereof
-#  
+#
 #  to be called on the path of a file just saved.
-#  
+#
 #  This can be used to process the file on disk, such as converting the notebook
 #  to a script or HTML via nbconvert.
-#  
+#
 #  It will be called as (all arguments passed by keyword)::
-#  
+#
 #      hook(os_path=os_path, model=model, contents_manager=instance)
-#  
+#
 #  - path: the filesystem path to the file just written - model: the model
 #  representing the file - contents_manager: this ContentsManager instance
 #c.FileContentsManager.post_save_hook = None
 
-## 
+##
 #c.FileContentsManager.root_dir = ''
 
 ## DEPRECATED, use post_save_hook. Will be removed in Notebook 5.0
