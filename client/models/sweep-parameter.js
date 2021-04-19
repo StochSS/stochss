40f2c6d82c0f37a36b758978c57d816beff43e2f
Bryan Rumsey <bmrumsey@gmail.com>
2021-04-19 12:52:57 -0400

    Refactored the parameter sweep settings to use a new sweep parameters collection.

--- client/models/sweep-parameter.js
+++ client/models/sweep-parameter.js
@@ -1,47 +1,39 @@
 /*
 StochSS is a platform for simulating biochemical systems
 Copyright (C) 2019-2020 StochSS developers.
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//collections
-let SweepParameters = require('./sweep-parameters');
 //models
-let State = require('ampersand-state');
-let Species = require('./specie');
+var State = require('ampersand-state');
 
 module.exports = State.extend({
-  children: {
-    speciesOfInterest: Species
-  },
-  collection: {
-    parameters: SweepParameters
+  props: {
+    paramID: 'number',
+    min: 'any',
+    max: 'any',
+    steps: 'any',
+    hasChangedRanged: 'boolean' 
   },
   initialize: function(attrs, options) {
     State.prototype.initialize.apply(this, arguments);
   },
-  updateVariables: function (parameters) {
-    this.parameters.forEach(function (variable) {
-      let parameter = parameters.filter(function (parameter) {
-        return parameter.compID === variable.paramID;
-      })[0];
-      if(parameter === undefined) {
-        this.removeVariable(variable);
-      }else{
-        variable.updateVariable(variable);
-      }
-    });
+  updateVariable: function (parameter) {
+    let value = parameter.expression;
+    if(this.min <= 0 || !this.hasChangedRanged) {
+      this.min = value * 0.5;
+    }
+    if(this.max <= 0 || !this.hasChangedRanged) {
+      this.max = value * 1.5;
+    }
   }
-}); 
+});
