b1fc71bce919e688de1d537f750e6fbb6585a333
Bryan Rumsey <bmrumsey@gmail.com>
2021-05-01 16:59:33 -0400

    Added function to controll exanding and collapsing of the user logs.  Added function to convert the datetime stamp to local time.  Added function to auto scroll to new logs unless user has recently scrolled the container.

--- client/views/main.js
+++ client/views/main.js
@@ -102,7 +102,7 @@ module.exports = View.extend({
   },
   events: {
     'click [data-hook=registration-link-button]' : 'handleRegistrationLinkClick',
-    'click [data-hook=user-logs-collapse]' : 'changeCollapseButtonText'
+    'click [data-hook=user-logs-collapse]' : 'collapseExpandLogs'
     //'click a[href]': 'handleLinkClick'
   },
   render: function () {
@@ -125,24 +125,81 @@ module.exports = View.extend({
 
     var homePath = window.location.pathname.startsWith("/user") ? "/hub/stochss" : "stochss/home"
     $(this.queryByHook("home-link")).prop('href', homePath);
+    let self = this;
     this.getUserLogs();
+    this.scrolled = false;
+    this.scrollCount = 0;
+    $("#user-logs").on("mousewheel", function(e) {
+      self.scrolled = true;
+      self.scrollCount = 0;
+    });
     return this;
   },
-  changeCollapseButtonText: function (e) {
-    app.changeCollapseButtonText(this, e);
+  collapseExpandLogs: function (e) {
+    let logs = $("#user-logs");
+    let classes = logs.attr("class").split(/\s+/);
+    if(classes.includes("show")) {
+      logs.removeClass("show");
+      $(this.queryByHook(e.target.dataset.hook)).html("+");
+      $(".user-logs").removeClass("expand-logs");
+      $(".side-navbar").css("z-index", 0);
+    }else{
+      logs.addClass("show");
+      $(this.queryByHook(e.target.dataset.hook)).html("-");
+      if($(".sidebar-sticky").css("position") === "fixed") {
+        $(".user-logs").addClass("expand-logs");
+        $(".side-navbar").css("z-index", 1);
+      }
+    }
+    // let element = document.querySelector("#user-logs");
+    // element.scrollTop = element.scrollHeight;
   },
   getUserLogs: function () {
     let self = this;
     let endpoint = path.join(app.getApiPath(), "user-logs");
     xhr({uri: endpoint, json: true}, function (err, response, body) {
-      if(response.statusCode < 400) {
-        logs = body;
+      if(response.statusCode < 400 && body) {
+        let scrolled = self.scrolled;
+        console.log(self.logs !== body)
+        if(!self.logs || self.logs.split("<br>").length !== body.split("<br>").length) {
+          console.log(self.logs, body)
+          self.formatLogs(body);
+          $("#user-logs").html(self.logs);
+          self.scrolled = scrolled;
+        }
+        if(!self.scrolled){
+          let element = document.querySelector("#user-logs");
+          element.scrollTop = element.scrollHeight;
+        }else if(self.scrollCount < 60) {
+          self.scrollCount += 1;
+        }else{
+          self.scrolled = false;
+          self.scrollCount = 0;
+        }
       }else{
-        logs = null;
+        self.logs = "";
       }
-      $("#user-logs").html(logs);
       self.updateUserLogs();
     });
+  },
+  formatLogs: function (logs) {
+    let logList = logs.split("<br>").map(function (log) {
+      var time = log.split('$ ')[0]
+      let date = new Date(time);
+      let months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+      var stamp = months[date.getMonth()] + " ";
+      stamp += date.getDate() + ", ";
+      stamp += date.getFullYear() + "  ";
+      let hours = date.getHours();
+      stamp += (hours < 10 ? 0 + hours : hours) + ":";
+      let minutes = date.getMinutes();
+      stamp += (minutes < 10 ? '0' + minutes : minutes) + ":";
+      let seconds = date.getSeconds();
+      stamp += seconds < 10 ? '0' + seconds : seconds;
+      return log.replace(time, stamp);
+    });
+    logs = logList.join("<br>")
+    this.logs = logs;
   },
   handleNewPage: function (view) {
     this.pageSwitcher.set(view);
