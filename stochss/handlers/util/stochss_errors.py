4661977ece19fc7b22baaa8dc45630382c6e8065
Bryan Rumsey <bmrumsey@gmail.com>
2021-05-22 12:29:38 -0400

    Refactored the result storage functions to catch and log errors instead of letting them propagate upward.  Jobs only error for result storage if all critical results storage processes fail.

--- stochss/handlers/util/stochss_errors.py
+++ stochss/handlers/util/stochss_errors.py
@@ -271,3 +271,24 @@ class PlotNotAvailableError(StochSSAPIError):
             Error traceback for the error
         '''
         super().__init__(406, "Plot Figure Not Available", msg, trace)
+
+
+class StochSSJobResultsError(StochSSAPIError):
+    '''
+    ################################################################################################
+    StochSS Job Results Error
+    ################################################################################################
+    '''
+
+    def __init__(self, msg, trace=None):
+        '''
+        Indicates that the job results object was corrupted
+
+        Attributes
+        ----------
+        msg : str
+            Details on what caused the error
+        trace : str
+            Error traceback for the error
+        '''
+        super().__init__(500, "Job Results Error", msg, trace)
