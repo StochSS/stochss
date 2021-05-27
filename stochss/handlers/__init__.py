4852ef0787dfa6ff9807a192bf78352c1e6e8ef5
Bryan Rumsey <bmrumsey@gmail.com>
2021-05-27 13:04:03 -0400

    Added api handler and route for publising model presentations.

--- stochss/handlers/__init__.py
+++ stochss/handlers/__init__.py
@@ -73,6 +73,7 @@ def get_page_handlers(route_start):
         (r"/stochss/api/model/to-sbml\/?", ModelToSBMLAPIHandler),
         (r"/stochss/api/model/run\/?", RunModelAPIHandler),
         (r"/stochss/api/model/exists\/?", ModelExistsAPIHandler),
+        (r"/stochss/api/model/presentation\/?", ModelPresentationAPIHandler),
         (r"/stochss/api/spatial-model/domain-list\/?", LoadExternalDomains),
         (r"/stochss/api/spatial-model/types-list\/?", LoadParticleTypesDescriptions),
         (r"/stochss/api/spatial-model/domain-plot\/?", LoadDomainAPIHandler),
