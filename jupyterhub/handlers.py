9e51e07430dc450965c6c9dd14ab036625152edd
Bryan Rumsey <bmrumsey@gmail.com>
2021-05-28 13:32:30 -0400

    pylint changes.

--- jupyterhub/handlers.py
+++ jupyterhub/handlers.py
@@ -1,14 +1,22 @@
 from jupyterhub.handlers.base import BaseHandler
 
 class HomeHandler(BaseHandler):
-
-  async def get(self):
-    html = self.render_template("stochss-home.html")
-    self.finish(html)
+    '''
+    ################################################################################################
+    Handler for rendering jupyterhub home page.
+    ################################################################################################
+    '''
+    async def get(self):
+        html = self.render_template("stochss-home.html")
+        self.finish(html)
 
 
 class JobPresentationHandler(BaseHandler):
-
-  async def get(self):
-    html = self.render_template("stochss-job-presentation.html")
-    self.finish(html)
+    '''
+    ################################################################################################
+    Handler for rendering jupyterhub job presentation page.
+    ################################################################################################
+    '''
+    async def get(self):
+        html = self.render_template("stochss-job-presentation.html")
+        self.finish(html)
