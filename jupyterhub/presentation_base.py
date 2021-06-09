ad0d1d157f84a830986e5c968d55495e23ef2154
Bryan Rumsey <bmrumsey@gmail.com>
2021-06-09 14:30:58 -0400

    Added the presentation base class to handle common backend functions for presentations.  Added the presentation error module to hold all of the presentation system backend errors.

---
+++ jupyterhub/presentation_base.py
@@ -0,0 +1,57 @@
+'''
+StochSS is a platform for simulating biochemical systems
+Copyright (C) 2019-2020 StochSS developers.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+
+class StochSSBase():
+    '''
+    ################################################################################################
+    StochSS base object
+    ################################################################################################
+    '''
+    user_dir = os.path.expanduser("~") # returns the path to the users home directory
+
+    def __init__(self, path):
+        '''
+        Intitialize a file object
+
+        Attributes
+        ----------
+        path : str
+            Path to the folder
+        '''
+        self.path = path
+        self.logs = []
+
+
+    def get_name(self, path=None):
+        '''
+        Get the name from the file object's path or provided path
+
+        Attributes
+        ----------
+        path : str
+            Path to a file object
+        '''
+        name = self.path if path is None else path
+        if name.endswith("/"):
+            name = name[:-1]
+        name = name.split('/').pop()
+        if "." not in name:
+            return name
+        return '.'.join(name.split('.')[:-1])
