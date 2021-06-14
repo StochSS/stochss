7963ffdca31a61cd42a46088c23ee057e0b485b4
Bryan Rumsey <bmrumsey@gmail.com>
2021-06-13 21:11:02 -0400

    Merged the advanced species section with the species listing section.  Updated the layout for the species section to allow the advanced species section to be merged into it.

--- client/views/species-editor.js
+++ client/views/species-editor.js
@@ -16,54 +16,50 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var $ = require('jquery');
+let $ = require('jquery');
 //support files
 let app = require('../app');
-var modals = require('../modals');
-var Tooltips = require('../tooltips');
+let Tooltips = require('../tooltips');
 //views
-var View = require('ampersand-view');
-var EditNonspatialSpecieView = require('./edit-specie');
-var EditSpatialSpecieView = require('./edit-spatial-specie');
-var EditAdvancedSpecie = require('./edit-advanced-specie');
+let View = require('ampersand-view');
+let SpecieView = require('./edit-species');
 //templates
-var nonspatialSpecieTemplate = require('../templates/includes/speciesEditor.pug');
-var spatialSpecieTemplate = require('../templates/includes/spatialSpeciesEditor.pug');
+let specieTemplate = require('../templates/includes/speciesEditor.pug');
 
 module.exports = View.extend({
   events: {
-    'change [data-hook=all-continuous]' : 'getDefaultSpeciesMode',
-    'change [data-hook=all-discrete]' : 'getDefaultSpeciesMode',
-    'change [data-hook=advanced]' : 'getDefaultSpeciesMode',
-    'click [data-hook=add-species]' : 'handleAddSpeciesClick',
-    'click [data-hook=save-species]' : 'switchToViewMode',
     'click [data-hook=collapse]' : 'changeCollapseButtonText',
+    'click [data-hook=add-species]' : 'handleAddSpeciesClick'
   },
   initialize: function (attrs, options) {
-    var self = this;
     View.prototype.initialize.apply(this, arguments);
-    this.baseModel = this.collection.parent;
-    this.tooltips = Tooltips.speciesEditor
+    this.template = attrs.spatial ? null : specieTemplate;
+    this.tooltips = Tooltips.speciesEditor;
+    this.defaultMode = attrs.defaultMode;
+    let self = this
     this.collection.on('update-species', function (compID, specie, isNameUpdate, isDefaultMode) {
-      self.collection.parent.reactions.map(function (reaction) {
-        reaction.reactants.map(function (reactant) {
+      self.collection.parent.reactions.forEach(function (reaction) {
+        reaction.reactants.forEach(function (reactant) {
           if(reactant.specie.compID === compID) {
             reactant.specie = specie;
           }
         });
-        reaction.products.map(function (product) {
+        reaction.products.forEach(function (product) {
           if(product.specie.compID === compID) {
             product.specie = specie;
           }
         });
         if(isNameUpdate) {
           reaction.buildSummary();
+          if(reaction.selected) {
+            self.parent.reactionsEditor.setDetailsView(reaction);
+          }
         }else if(!isDefaultMode || specie.compID === self.collection.models[self.collection.length-1].compID){
           reaction.checkModes();
         }
       });
-      self.collection.parent.eventsCollection.map(function (event) {
-        event.eventAssignments.map(function (assignment) {
+      self.collection.parent.eventsCollection.forEach(function (event) {
+        event.eventAssignments.forEach(function (assignment) {
           if(assignment.variable.compID === compID) {
             assignment.variable = specie;
           }
@@ -72,127 +68,20 @@ module.exports = View.extend({
           event.detailsView.renderEventAssignments();
         }
       });
-      self.collection.parent.rules.map(function (rule) {
+      self.collection.parent.rules.forEach(function (rule) {
         if(rule.variable.compID === compID) {
           rule.variable = specie;
         }
       });
       if(isNameUpdate) {
-        self.renderSpeciesAdvancedView();
         self.parent.renderRulesView();
       }
     });
   },
   render: function () {
-    this.template = this.parent.model.is_spatial ? spatialSpecieTemplate : nonspatialSpecieTemplate;
     View.prototype.render.apply(this, arguments);
-    var defaultMode = this.collection.parent.defaultMode;
-    if(defaultMode === "" && !this.collection.parent.is_spatial){
-      this.getInitialDefaultSpeciesMode();
-    }else{
-      var dataHooks = {'continuous':'all-continuous', 'discrete':'all-discrete', 'dynamic':'advanced'}
-      $(this.queryByHook(dataHooks[this.collection.parent.defaultMode])).prop('checked', true)
-      if(defaultMode === "dynamic"){
-        $(this.queryByHook('advanced-species')).collapse('show');
-      }
-    }
-    this.renderEditSpeciesView();
-    this.renderSpeciesAdvancedView();
     this.toggleSpeciesCollectionError();
-  },
-  update: function () {
-  },
-  updateValid: function (e) {
-  },
-  getInitialDefaultSpeciesMode: function () {
-    var self = this;
-    if(document.querySelector('#defaultModeModal')) {
-      document.querySelector('#defaultModeModal').remove()
-    }
-    let modal = $(modals.renderDefaultModeModalHtml()).modal();
-    let continuous = document.querySelector('#defaultModeModal .concentration-btn');
-    let discrete = document.querySelector('#defaultModeModal .population-btn');
-    let dynamic = document.querySelector('#defaultModeModal .hybrid-btn');
-    continuous.addEventListener('click', function (e) {
-      self.setInitialDefaultMode(modal, "continuous");
-    });
-    discrete.addEventListener('click', function (e) {
-      self.setInitialDefaultMode(modal, "discrete");
-    });
-    dynamic.addEventListener('click', function (e) {
-      self.setInitialDefaultMode(modal, "dynamic");
-    });
-  },
-  setInitialDefaultMode: function (modal, mode) {
-    var dataHooks = {'continuous':'all-continuous', 'discrete':'all-discrete', 'dynamic':'advanced'}
-    modal.modal('hide')
-    $(this.queryByHook(dataHooks[mode])).prop('checked', true)
-    this.setAllSpeciesModes(mode)
-  },
-  getDefaultSpeciesMode: function (e) {
-    var self = this;
-    this.setAllSpeciesModes(e.target.dataset.name, function (specie) {
-      self.collection.trigger('update-species', specie.compID, specie, false, true)
-    });
-  },
-  setAllSpeciesModes: function (defaultMode, cb) {
-    this.collection.parent.defaultMode = defaultMode;
-    this.collection.forEach(function (specie) { 
-      specie.mode = defaultMode
-      if(cb) {
-        cb(specie)
-      }
-    });
-    if(!this.collection.parent.is_spatial) {
-      if(defaultMode === "continuous") {
-        $(this.parent.queryByHook("system-volume-container")).collapse("hide")
-      }else{
-        $(this.parent.queryByHook("system-volume-container")).collapse("show")
-      }
-      if(defaultMode === "dynamic"){
-        this.renderSpeciesAdvancedView()
-        $(this.queryByHook('advanced-species')).collapse('show');
-      }
-      else{
-        this.speciesAdvancedView.views[0].updateInputValidation()
-        $(this.queryByHook('advanced-species')).collapse('hide');
-      }
-    }
-  },
-  renderEditSpeciesView: function () {
-    if(this.editSpeciesView){
-      this.editSpeciesView.remove();
-    }
-    var editSpecieView = !this.collection.parent.is_spatial ? EditNonspatialSpecieView : EditSpatialSpecieView;
-    this.editSpeciesView = this.renderCollection(
-      this.collection,
-      editSpecieView,
-      this.queryByHook('specie-list')
-    );
-    $(document).ready(function () {
-      $('[data-toggle="tooltip"]').tooltip();
-      $('[data-toggle="tooltip"]').click(function () {
-        $('[data-toggle="tooltip"]').tooltip("hide");
-      });
-    });
-  },
-  renderSpeciesAdvancedView: function () {
-    if(this.collection.parent.is_spatial) {
-      return
-    }
-    if(this.speciesAdvancedView) {
-      this.speciesAdvancedView.remove()
-    }
-    this.speciesAdvancedView = this.renderCollection(this.collection, EditAdvancedSpecie, this.queryByHook('edit-species-mode'));
-  },
-  handleAddSpeciesClick: function (e) {
-    var self = this;
-    var defaultMode = this.collection.parent.defaultMode;
-    if(defaultMode === "" && !this.collection.parent.is_spatial){
-      this.getInitialDefaultSpeciesMode();
-    }else{
-      this.addSpecies();
-    }
+    this.renderEditSpeciesView();
   },
   addSpecies: function () {
     if(this.parent.model.domain.types) {
@@ -213,6 +102,28 @@ module.exports = View.extend({
        });
     });
   },
+  changeCollapseButtonText: function (e) {
+    app.changeCollapseButtonText(this, e);
+  },
+  handleAddSpeciesClick: function (e) {
+    let self = this;
+    let defaultMode = this.collection.parent.defaultMode;
+    if(defaultMode === "" && !this.collection.parent.is_spatial){
+      this.parent.getInitialDefaultMode();
+    }else{
+      this.addSpecies();
+    }
+  },
+  renderEditSpeciesView: function () {
+    if(this.editSpeciesView){
+      this.editSpeciesView.remove();
+    }
+    this.editSpeciesView = this.renderCollection(
+      this.collection,
+      SpecieView,
+      this.queryByHook('specie-list')
+    );
+  },
   toggleSpeciesCollectionError: function () {
     let errorMsg = $(this.queryByHook('species-collection-error'))
     if(this.collection.length <= 0) {
@@ -222,12 +133,5 @@ module.exports = View.extend({
       errorMsg.addClass('component-valid')
       errorMsg.removeClass('component-invalid')
     }
-  },
-  switchToViewMode: function (e) {
-    this.parent.modelStateButtons.clickSaveHandler(e);
-    this.parent.renderSpeciesView(mode="view");
-  },
-  changeCollapseButtonText: function (e) {
-    app.changeCollapseButtonText(this, e);
   }
 });
