7589ffc53503510a11d3ec77cd2a8ee2f50aa42c
Bryan Rumsey <bmrumsey@gmail.com>
2021-06-13 21:07:25 -0400

    Moved the models mode section and all functions from the variables section to the model editor page.

--- client/pages/model-editor.js
+++ client/pages/model-editor.js
@@ -23,6 +23,7 @@ let path = require('path');
 var app = require('../app');
 var modals = require('../modals');
 var tests = require('../views/tests');
+let Tooltips = require("../tooltips");
 //views
 var PageView = require('../pages/base');
 var InputView = require('../views/input');
@@ -53,6 +54,9 @@ import initPage from './page.js';
 let ModelEditor = PageView.extend({
   template: template,
   events: {
+    'change [data-hook=all-continuous]' : 'setDefaultMode',
+    'change [data-hook=all-discrete]' : 'setDefaultMode',
+    'change [data-hook=advanced]' : 'setDefaultMode',
     'click [data-hook=edit-model-help]' : function () {
       let modal = $(modals.operationInfoModalHtml('model-editor')).modal();
     },
@@ -65,6 +69,7 @@ let ModelEditor = PageView.extend({
   },
   initialize: function (attrs, options) {
     PageView.prototype.initialize.apply(this, arguments);
+    this.tooltips = Tooltips.modelEditor
     var self = this;
     let urlParams = new URLSearchParams(window.location.search)
     var directory = urlParams.get('path');
@@ -217,6 +222,12 @@ let ModelEditor = PageView.extend({
     }
   },
   renderSubviews: function () {
+    if(this.model.defaultMode === "" && !this.model.is_spatial){
+      this.getInitialDefaultMode();
+    }else{
+      let dataHooks = {'continuous':'all-continuous', 'discrete':'all-discrete', 'dynamic':'advanced'}
+      $(this.queryByHook(dataHooks[this.model.defaultMode])).prop('checked', true)
+    }
     this.modelSettings = new TimespanSettingsView({
       parent: this,
       model: this.model.modelSettings,
@@ -292,7 +303,8 @@ let ModelEditor = PageView.extend({
     }
     this.speciesEditor = new SpeciesEditorView({
       collection: this.model.species,
-      spatial: this.model.is_spatial
+      spatial: this.model.is_spatial,
+      defaultMode: this.model.defaultMode
     });
     app.registerRenderSubview(this, this.speciesEditor, 'species-editor-container');
   },
@@ -423,6 +435,63 @@ let ModelEditor = PageView.extend({
   clickDownloadPNGButton: function (e) {
     let pngButton = $('div[data-hook=preview-plot-container] a[data-title*="Download plot as a png"]')[0]
     pngButton.click()
+  },
+  getInitialDefaultMode: function () {
+    var self = this;
+    if(document.querySelector('#defaultModeModal')) {
+      document.querySelector('#defaultModeModal').remove();
+    }
+    let modal = $(modals.renderDefaultModeModalHtml()).modal();
+    let continuous = document.querySelector('#defaultModeModal .concentration-btn');
+    let discrete = document.querySelector('#defaultModeModal .population-btn');
+    let dynamic = document.querySelector('#defaultModeModal .hybrid-btn');
+    continuous.addEventListener('click', function (e) {
+      self.setInitialDefaultMode(modal, "continuous");
+    });
+    discrete.addEventListener('click', function (e) {
+      self.setInitialDefaultMode(modal, "discrete");
+    });
+    dynamic.addEventListener('click', function (e) {
+      self.setInitialDefaultMode(modal, "dynamic");
+    });
+  },
+  setAllSpeciesModes: function (prevMode) {
+    let self = this;
+    this.model.species.forEach(function (specie) { 
+      specie.mode = self.model.defaultMode;
+      self.model.species.trigger('update-species', specie.compID, specie, false, true);
+    });
+    let switchToDynamic = (!Boolean(prevMode) || prevMode !== "dynamic") && this.model.defaultMode === "dynamic";
+    let switchFromDynamic = Boolean(prevMode) && prevMode === "dynamic" && this.model.defaultMode !== "dynamic";
+    // if(switchToDynamic || switchFromDynamic) {
+    //   this.speciesEditor.();
+    // }
+  },
+  setDefaultMode: function (e) {
+    let self = this;
+    let prevMode = this.model.defaultMode;
+    this.model.defaultMode = e.target.dataset.name;
+    this.speciesEditor.defaultMode = e.target.dataset.name;
+    this.setAllSpeciesModes(prevMode);
+    this.toggleVolumeContainer();
+  },
+  setInitialDefaultMode: function (modal, mode) {
+    var dataHooks = {'continuous':'all-continuous', 'discrete':'all-discrete', 'dynamic':'advanced'};
+    modal.modal('hide');
+    $(this.queryByHook(dataHooks[mode])).prop('checked', true);
+    this.model.defaultMode = mode;
+    this.speciesEditor.defaultMode = mode;
+    this.setAllSpeciesModes();
+    this.toggleVolumeContainer();
+  },
+  toggleVolumeContainer: function () {
+    if(!this.model.is_spatial) {
+      if(this.model.defaultMode === "continuous") {
+        $(this.queryByHook("system-volume-container")).collapse("hide");
+      }else{
+        $(this.queryByHook("system-volume-container")).collapse("show");
+      }
+    }
   }
 });
 
