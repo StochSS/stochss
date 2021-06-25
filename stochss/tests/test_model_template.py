1572cb6b54fd8afef89f3635db8e31c88963e6e7
Bryan Rumsey <bmrumsey@gmail.com>
2021-06-25 12:15:40 -0400

    Added a test for the domain in the model template.  Pylint changes.

--- stochss/tests/test_model_template.py
+++ stochss/tests/test_model_template.py
@@ -1,12 +1,37 @@
+'''
+StochSS is a platform for simulating biochemical systems
+Copyright (C) 2019-2020 StochSS developers.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import unittest
 import json
 import os
 
 os.chdir('/stochss')
 
-class TestNonSpatialModelTemplate(unittest.TestCase):
-
+class TestModelTemplate(unittest.TestCase):
+    '''
+    ################################################################################################
+    Unit tests for model template complete ness.
+    ################################################################################################
+    '''
     def setUp(self):
+        '''
+        Get the model template prior to each test.
+        '''
         template_path = "stochss_templates/nonSpatialModelTemplate.json"
 
         with open(template_path, "r") as template_file:
@@ -14,6 +39,9 @@ class TestNonSpatialModelTemplate(unittest.TestCase):
 
 
     def test_model_elements(self):
+        '''
+        Check if the model template has all of the properties currently in the model.
+        '''
         template_keys = sorted(list(self.template.keys()))
         model_path = "client/models/model.js"
 
@@ -30,12 +58,35 @@ class TestNonSpatialModelTemplate(unittest.TestCase):
         self.assertEqual(template_keys, model_keys)
 
 
-    def test_model_settings_elements(self):
+    def test_timespan_settings_elements(self):
+        '''
+        Check if the timespan settings in the model template has
+        all of the properties currently in the timespan settings.
+        '''
         template_keys = sorted(list(self.template['modelSettings'].keys()))
-        mdl_settings_path = "client/models/model-settings.js"
+        mdl_settings_path = "client/models/timespan-settings.js"
 
         with open(mdl_settings_path, "r") as mdl_settings_file:
             data = mdl_settings_file.read().split("props: {").pop().split('}')[0].split(',')
             mdl_settings_keys = sorted(list(map(lambda item: item.strip().split(':')[0], data)))
 
         self.assertEqual(template_keys, mdl_settings_keys)
+
+
+    def test_model_domain_elements(self):
+        '''
+        Check if the domain in the model template has all of the
+        properties currently in the domain.
+        '''
+        template_keys = sorted(list(self.template['domain'].keys()))
+        domain_path = "client/models/domain.js"
+
+        with open(domain_path, "r") as domain_file:
+            data = domain_file.read()
+            props = data.split("props: {").pop().split('}')[0].split(',')
+            collections = data.split("collections: {").pop().split('}')[0].split(',')
+            domain_keys = list(map(lambda item: item.strip().split(':')[0], props))
+            domain_keys.extend(list(map(lambda item: item.strip().split(':')[0], collections)))
+
+        domain_keys.sort()
+        self.assertEqual(template_keys, domain_keys)
