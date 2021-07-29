2d75a13d013fe351e2717ebebc5d8f9e0dc9a5c9
Bryan Rumsey <bmrumsey@gmail.com>
2021-07-29 11:25:07 -0400

    Fixed the copy link button.

--- client/app.js
+++ client/app.js
@@ -213,28 +213,13 @@ documentSetup = () => {
 }
 
 copyToClipboard = (text) => {
-    if (window.clipboardData && window.clipboardData.setData) {
-        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
-        return window.clipboardData.setData("Text", text);
-
-    }
-    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-        var textarea = document.createElement("textarea");
-        textarea.textContent = text;
-        textarea.style.display = 'none';  // Prevent scrolling to bottom of page in Microsoft Edge.
-        document.body.appendChild(textarea);
-        textarea.select();
-        try {
-            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
-        }
-        catch (ex) {
-            console.warn("Copy to clipboard failed.", ex);
-            return false;
-        }
-        finally {
-            document.body.removeChild(textarea);
-        }
-    }
+  if (window.clipboardData && window.clipboardData.setData) {
+    // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+    return window.clipboardData.setData("Text", text);
+  }
+  else {
+    navigator.clipboard.writeText(text)
+  }
 }
 
 module.exports = {
