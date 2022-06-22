6665f109098c3cc41218de16104c54af1f6a62b3
Bryan Rumsey <bmrumsey@gmail.com>
2022-06-22 12:39:59 -0400

    Refactored to support the new stoich species view and reaction view.

--- client/model-view/views/reactant-product.js
+++ client/model-view/views/reactant-product.js
@@ -17,59 +17,52 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 let $ = require('jquery');
-//models
-let StoichSpecie = require('../../models/stoich-specie');
+//support files
+let app = require('../../app');
 //views
 let View = require('ampersand-view');
 let SelectView = require('ampersand-select-view');
-let EditStoichSpecieView = require('./edit-stoich-specie');
-let EditCustomStoichSpecieView = require('./edit-custom-stoich-specie');
+let StoichSpeciesView = require('./stoich-species-view');
 //templates
 let template = require('../templates/reactantProduct.pug');
 
 module.exports = View.extend({
   template: template,
-  events: {
-    'change [data-hook=select-specie]' : 'selectSpecie',
-    'click [data-hook=add-selected-specie]' : 'addSelectedSpecie'
+  events: function () {
+    let events = {};
+    events[`change [data-hook=${this.hookAnchor}-select-specie]`] = 'selectSpecie';
+    events[`click [data-hook=${this.hookAnchor}-add-selected-specie]`] = 'addSelectedSpecie';
+    return events;
   },
   initialize: function (args) {
     View.prototype.initialize.apply(this, arguments);
     this.collection = args.collection;
     this.species = args.species;
     this.reactionType = args.reactionType;
-    this.isReactants = args.isReactants
+    this.custom = args.reactionType.startsWith('custom');
+    this.isReactants = args.isReactants;
     this.unselectedText = 'Pick a species';
-    this.fieldTitle = args.fieldTitle;
+    if(this.isReactants) {
+      this.fieldTitle = 'Reactants';
+      this.hookAnchor = 'reactants';
+    }else{
+      this.fieldTitle = 'Products';
+      this.hookAnchor = 'products';
+    }
   },
   render: function () {
     View.prototype.render.apply(this, arguments);
-    let args = {
-      viewOptions: {
-        name: 'stoich-specie',
-        required: true,
-        textAttribute: 'name',
-        eagerValidate: true,
-        idAttribute: 'name',
-        options: this.species
-      }
-    };
-    let type = this.reactionType;
-    let StoichSpeciesView = (type.startsWith('custom')) ? EditCustomStoichSpecieView : EditStoichSpecieView;
-    this.renderCollection(
-        this.collection,
-        StoichSpeciesView,
-        this.queryByHook('reactants-editor'),
-        args
-    );
-    if(this.reactionType.startsWith('custom')) {
-      $(this.queryByHook('collapse')).collapse();
-    }
-    this.toggleAddSpecieButton();
-    if(this.fieldTitle === "Reactants"){
-      $(this.queryByHook('field-title-tooltip')).prop('title', this.parent.parent.tooltips.reactant);
+    if(this.isReactants) {
+      var tooltip = this.parent.parent.tooltips.reactant;
     }else{
-      $(this.queryByHook('field-title-tooltip')).prop('title', this.parent.parent.tooltips.product);
+      var tooltip = this.parent.parent.tooltips.product;
+    }
+    $(this.queryByHook('field-title-tooltip')).prop('title', tooltip);
+    this.renderStoichSpecies();
+    if(this.custom) {
+      $(this.queryByHook(`custom-${this.hookAnchor}`)).css('display', 'block');
+      this.renderSelectSpeciesView();
+      this.toggleAddSpecieButton();
     }
   },
   addSelectedSpecie: function () {
@@ -79,6 +72,41 @@ module.exports = View.extend({
       this.toggleAddSpecieButton();
       this.collection.parent.trigger('change-reaction');
     }
+  },
+  renderSelectSpeciesView: function () {
+    if(this.selectSpeciesView) {
+      this.selectSpeciesView.remove();
+    }
+
+    this.selectSpeciesView = new SelectView({
+      name: 'stoich-specie',
+      required: false,
+      textAttribute: 'name',
+      eagerValidate: false,
+      idAttribute: 'compID',
+      options: this.species,
+      unselectedText: this.unselectedText
+    });
+    app.registerRenderSubview(this, this.selectSpeciesView, `${this.hookAnchor}-select-specie`);
+  },
+  renderStoichSpecies: function () {
+    let args = {
+      viewOptions: {
+        name: 'stoich-specie',
+        required: true,
+        textAttribute: 'name',
+        eagerValidate: true,
+        idAttribute: 'compID',
+        yieldModel: false,
+        options: this.species
+      }
+    };
+    this.renderCollection(
+        this.collection,
+        StoichSpeciesView,
+        this.queryByHook(`${this.hookAnchor}-editor`),
+        args
+    );
   },
   selectSpecie: function (e) {
     if(this.unselectedText === e.target.selectedOptions.item(0).text){
@@ -90,37 +118,17 @@ module.exports = View.extend({
     this.toggleAddSpecieButton();
   },
   toggleAddSpecieButton: function () {
-    if(!this.validateAddSpecie()){
-      $(this.queryByHook('add-selected-specie')).prop('disabled', true);
-    }else{
-      $(this.queryByHook('add-selected-specie')).prop('disabled', false);
-    }
+    $(this.queryByHook(`${this.hookAnchor}-add-selected-specie`)).prop('disabled', !this.validateAddSpecie());
     this.parent.toggleCustomReactionError();
   },
   validateAddSpecie: function () {
     if(this.hasSelectedSpecie){
       if(!this.collection.length) { return true; }
       if(this.collection.length < 2 && this.collection.at(0).ratio < 2) { return true; }
-      if(this.reactionType !== 'custom-massaction') { return true; }
+      if(this.reactionType === 'custom-propensity') { return true; }
       if(!this.isReactants) { return true; }
       return false;
     }
     return false;
   },
-  subviews: {
-    selectSpecies: {
-      hook: 'select-specie',
-      prepareView: function (el) {
-        return new SelectView({
-          name: 'stoich-specie',
-          required: false,
-          textAttribute: 'name',
-          eagerValidate: false,
-          idAttribute: 'name',
-          options: this.species,
-          unselectedText: this.unselectedText
-        });
-      }
-    }
-  }
 });
