21e4d49e4e36efca4ffa960ca7a14ce82d8bb700
Bryan Rumsey <bmrumsey@gmail.com>
2022-10-13 12:23:09 -0400

    Updated the tooltip for geometry formulas.

--- client/tooltips.js
+++ client/tooltips.js
@@ -183,10 +183,10 @@ module.exports = {
 
     speed: "Approximate or artificial speed of sound"
   },
-  domainType: {
-    geometry: "The geometry expression can be any mathematical expression which evaluates to a boolean value in a python environment (i.e. x==5).  This "+
-        "expression is evaluable within the a limited namespace, and only lower case variables (x, y, z), particles location, and (cx, cy, cz), "+
-        "center of the geometry, can be referenced in the expression."
+  domainGeometry: {
+    geometry: "The geometry formula can be any mathematical expression which evaluates to a boolean value in a python environment (i.e. x==5).  This "+
+        "expression is evaluable within the a limited namespace, and only lower case variables (x, y, z), particles location, "+
+        "can be referenced in the expression."
   },
   boundaryConditionsEditor: {
     annotation: "An optional note about a boundary condition."
