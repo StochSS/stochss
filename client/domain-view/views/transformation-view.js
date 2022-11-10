48d599b6caca95dcee04a6eecc7fe734f8739f63
Bryan Rumsey <bmrumsey@gmail.com>
2022-11-10 16:19:56 -0500

    Created the basic transformation view and template files.

---
+++ client/domain-view/views/transformation-view.js
@@ -0,0 +1,38 @@
+/*
+StochSS is a platform for simulating biochemical systems
+Copyright (C) 2019-2022 StochSS developers.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//views
+let View = require('ampersand-view');
+//templates
+let editTemplate = require('../templates/editTransformation.pug');
+let viewTemplate = require('../templates/viewTransformation.pug');
+
+module.exports = View.extend({
+  bindings: {
+  },
+  events: {
+  },
+  initialize: function (attrs, options) {
+    View.prototype.initialize.apply(this, arguments);
+    this.viewMode = attrs.viewMode ? attrs.viewMode : false;
+  },
+  render: function () {
+    this.template = this.viewMode ? viewTemplate : editTemplate;
+    View.prototype.render.apply(this, arguments);
+  }
+});
