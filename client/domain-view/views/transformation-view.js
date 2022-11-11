b9bec3b81f1c7ccbbdef7f927d6feaa46b8a14ee
Bryan Rumsey <bmrumsey@gmail.com>
2022-11-11 14:25:26 -0500

    Added all common elements for transformation models.

--- client/domain-view/views/transformation-view.js
+++ client/domain-view/views/transformation-view.js
@@ -16,23 +16,189 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+let $ = require('jquery');
+let _ = require('underscore');
+//support files
+let app = require('../../app');
+let tests = require('../../views/tests');
 //views
 let View = require('ampersand-view');
+let InputView = require('../../views/input');
+let SelectView = require('ampersand-select-view');
 //templates
 let editTemplate = require('../templates/editTransformation.pug');
 let viewTemplate = require('../templates/viewTransformation.pug');
 
 module.exports = View.extend({
   bindings: {
+    'model.selected' : {
+      type: function (el, value, previousValue) {
+        el.checked = value;
+      },
+      hook: 'select-transformation'
+    }
   },
   events: {
+    'change [data-hook=select-type-container]' : 'selectTransformationType',
+    'change [data-hook=geometry-select-container]' : 'selectNestedGeometry',
+    'change [data-hook=lattice-select-container]' : 'selectNestedLattice',
+    'change [data-hook=transformation-select-container]' : 'selectNestedTransformation',
+    'click [data-hook=select-transformation]' : 'selectTransformation',
+    'click [data-hook=remove]' : 'removeTransformation'
   },
   initialize: function (attrs, options) {
     View.prototype.initialize.apply(this, arguments);
     this.viewMode = attrs.viewMode ? attrs.viewMode : false;
+    this.details = {
+      'Translate Transformation': [],
+      'Rotate Transformation': [],
+      'Reflect Transformation': [],
+      'Scale Transformation': []
+    }
   },
   render: function () {
     this.template = this.viewMode ? viewTemplate : editTemplate;
     View.prototype.render.apply(this, arguments);
+    app.documentSetup();
+    if(!this.viewMode){
+      if(this.model.selected) {
+        setTimeout(_.bind(this.openDetails, this), 1);
+      }
+      this.model.on('change', _.bind(this.updateViewer, this));
+      // TODO: Need an event listener to update the geometry select when a geometry name is changed
+      // TODO: Need an event listener to update the lattice select when a lattice name is changed
+      // TODO: Need an event listener to update the transformation select when a transformation name is changed
+      this.renderGeometrySelectView();
+      this.renderLatticeSelectView();
+      this.renderTransformationSelectView();
+    }
+    this.displayDetails();
+  },
+  displayDetails: function () {
+    this.details[this.model.type].forEach((element) => {
+      element.css('display', 'block');
+    });
+  },
+  hideDetails: function () {
+    this.details[this.model.type].forEach((element) => {
+      element.css('display', 'none');
+    });
+  },
+  openDetails: function () {
+    $("#collapse-transformation-details" + this.model.cid).collapse("show");
+  },
+  removeTransformation: function () {
+    this.model.collection.removeTransformation(this.model);
+  },
+  renderGeometrySelectView: function () {
+    if(this.geometrySelectView) {
+      this.geometrySelectView.remove();
+    }
+    let options = this.model.collection.parent.geometries;
+    this.geometrySelectView = new SelectView({
+      name: 'geometry',
+      required: false,
+      idAttribute: 'name',
+      textAttribute: 'name',
+      eagerValidate: false,
+      options: options,
+      value: this.model.geometry,
+      unselectedText: '-- Select a Geometry --'
+    });
+    let hook = "geometry-select-container";
+    app.registerRenderSubview(this, this.geometrySelectView, hook);
+  },
+  renderLatticeSelectView: function () {
+    if(this.latticeSelectView) {
+      this.latticeSelectView.remove();
+    }
+    let options = this.model.collection.parent.lattices;
+    this.latticeSelectView = new SelectView({
+      name: 'lattice',
+      required: false,
+      idAttribute: 'name',
+      textAttribute: 'name',
+      eagerValidate: false,
+      options: options,
+      value: this.model.lattice,
+      unselectedText: '-- Select a Lattice --'
+    });
+    let hook = "lattice-select-container";
+    app.registerRenderSubview(this, this.latticeSelectView, hook);
+  },
+  renderTransformationSelectView: function () {
+    if(this.transformationSelectView) {
+      this.transformationSelectView.remove();
+    }
+    let options = [];
+    this.model.collection.parent.transformations.forEach((transformation) => {
+      if(transformation.name !== this.model.name) {
+        options.push(transformation.name);
+      }
+    });
+    this.transformationSelectView = new SelectView({
+      name: 'transformation',
+      required: false,
+      eagerValidate: false,
+      options: options,
+      value: this.model.transformation,
+      unselectedText: '-- Select a Transformation --'
+    });
+    let hook = "transformation-select-container";
+    app.registerRenderSubview(this, this.transformationSelectView, hook);
+  },
+  selectNestedGeometry: function (e) {
+    this.model.geometry = e.target.value;
+  },
+  selectNestedLattice: function (e) {
+    this.model.lattice = e.target.value;
+  },
+  selectNestedTransformation: function (e) {
+    this.model.transformation = e.target.value;
+  },
+  selectTransformation: function () {
+    this.model.selected = !this.model.selected;
+  },
+  selectTransformationType: function (e) {
+    this.hideDetails();
+    this.model.type = e.target.value;
+    this.displayDetails();
+  },
+  updateViewer: function () {
+    this.parent.renderViewTransformationsView();
+  },
+  subviews: {
+    inputNameView: {
+      hook: 'input-name-container',
+      prepareView: function (el) {
+        return new InputView({
+          parent: this,
+          required: true,
+          name: 'name',
+          tests: tests.nameTests,
+          modelKey: 'name',
+          valueType: 'string',
+          value: this.model.name
+        });
+      }
+    },
+    selectTypeView: {
+      hook: 'select-type-container',
+      prepareView: function (el) {
+        let options = [
+          'Translate Transformation',
+          'Rotate Transformation',
+          'Reflect Transformation',
+          'Scale Transformation'
+        ];
+        return new SelectView({
+          name: 'type',
+          required: true,
+          idAttributes: 'cid',
+          options: options,
+          value: this.model.type
+        });
+      }
+    }
   }
 });
