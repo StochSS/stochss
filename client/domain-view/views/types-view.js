9cdca6c6f85aff6504dea542da50e2bebcf6887a
Bryan Rumsey <bmrumsey@gmail.com>
2022-12-14 16:13:02 -0500

    fixed issue with adding types.

--- client/domain-view/views/types-view.js
+++ client/domain-view/views/types-view.js
@@ -60,7 +60,7 @@ module.exports = View.extend({
   handleAddDomainType: function () {
     let name = this.collection.addType();
     this.collection.parent.actions.trigger('update-type-options', {currName: name});
-    this.parent.addType(name);
+    // this.parent.addType(name);
   },
   renderEditTypeView: function () {
     if(this.editTypeView) {
