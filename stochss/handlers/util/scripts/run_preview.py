c785cd72ed8c557ff4dea955b16959e0c5780c80
Bryan Rumsey <bmrumsey@gmail.com>
2023-02-13 15:36:00 -0500

    Updated the copyright end year to 2023.

--- stochss/handlers/util/scripts/run_preview.py
+++ stochss/handlers/util/scripts/run_preview.py
@@ -2,7 +2,7 @@
 
 '''
 StochSS is a platform for simulating biochemical systems
-Copyright (C) 2019-2022 StochSS developers.
+Copyright (C) 2019-2023 StochSS developers.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
