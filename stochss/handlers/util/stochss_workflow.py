e56ffdaffc0202b628e1dcba64f2fb24b62c77cc
Bryan Rumsey <bmrumsey@gmail.com>
2023-02-16 15:22:53 -0500

    Fixed issue cause project loading to fail when a workflows model path is None.

--- stochss/handlers/util/stochss_workflow.py
+++ stochss/handlers/util/stochss_workflow.py
@@ -332,7 +332,8 @@ class StochSSWorkflow(StochSSBase):
             self.workflow['type'] = jobdata['titleType']
             oldfmtrdy = jobdata['status'] == "ready"
         self.__update_settings()
-        if not os.path.exists(self.workflow['model']) and (oldfmtrdy or self.workflow['newFormat']):
+        if (self.workflow['model'] is None or not os.path.exists(self.workflow['model'])) \
+                                                    and (oldfmtrdy or self.workflow['newFormat']):
             if ".proj" in self.path:
                 if "WorkflowGroup1.wkgp" in self.path:
                     proj = StochSSFolder(path=os.path.dirname(self.get_dir_name(full=True)))
