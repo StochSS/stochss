cc0236aa7c43e4260db5bce427c3cedd4392f729
Bryan Rumsey <bmrumsey@gmail.com>
2023-03-08 16:30:22 -0500

    Split the test suite into unit tests and integration tests.

--- stochss/tests/run_unit_tests.py
+++ stochss/tests/run_unit_tests.py
@@ -17,43 +17,44 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
-
-
-import unittest
-import sys
 import os
+import sys
+import unittest
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--mode', default='staging', choices=['staging', 'release'],
-                    help='Run tests in staging mode or release mode.')
+parser.add_argument(
+    '-m', '--mode', default='staging', choices=['staging', 'release'],
+    help='Run unit tests in staging mode or release mode.'
+)
 
-if __name__ == "__main__":
-    os.chdir('/stochss')
-    args = parser.parse_args()
-    print(os.path.dirname(__file__))
-
-    import test_model_template
-    import test_settings_template
-    import test_stochss_base
-    import test_stochss_file
-    import test_gillespy2
-
+def run(mode=None):
+    ''' Run the unit tests. '''
+    from unit_tests import test_model_template
+    from unit_tests import test_settings_template
+    from unit_tests import test_stochss_base
+    from unit_tests import test_stochss_file
+    
     modules = [
         test_model_template,
         test_settings_template,
-        test_gillespy2,
         test_stochss_base,
         test_stochss_file
     ]
 
     for module in modules:
         suite = unittest.TestLoader().loadTestsFromModule(module)
-        runner = unittest.TextTestRunner(failfast=args.mode == 'staging', verbosity=1)
+        runner = unittest.TextTestRunner(failfast=mode == 'staging', verbosity=1)
 
         print("Executing: {}".format(module))
         result = runner.run(suite)
         print('=' * 70)
         if not result.wasSuccessful():
             sys.exit(not result.wasSuccessful())
+
+if __name__ == "__main__":
+    os.chdir('/stochss')
+    args = parser.parse_args()
+    print(os.path.dirname(__file__))
+
+    run(mode=args.mode)
