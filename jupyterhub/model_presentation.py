7f501c185306c77542aa850ce4cada170be27dee
Bryan Rumsey <bmrumsey@gmail.com>
2023-04-12 13:56:38 -0400

    Added debug statements.

--- jupyterhub/model_presentation.py
+++ jupyterhub/model_presentation.py
@@ -193,14 +193,14 @@ class StochSSModel(StochSSBase):
         '''
         super().__init__()
         self.model = model
+        self.diff = []
         um_lines = json.dumps(template.update(model), sort_keys=True, indent=4).split("\n")
-        m_lines = json.dumps(template.update(model), sort_keys=True, indent=4).split("\n")
+        m_lines = json.dumps(model, sort_keys=True, indent=4).split("\n")
         for i, um_line in enumerate(um_lines):
-            print(um_line.ljust(65), end=" | ")
+            diff = f"{um_line.ljust(65)} | "
             if m_lines[i] == um_line:
-                print(m_lines[i])
-            else:
-                print()
+                diff += m_lines[i]
+            self.diff.append(diff)
 
     @classmethod
     def __update_event_assignments(cls, event, param_ids):
@@ -298,7 +298,7 @@ class StochSSModel(StochSSBase):
             self.model['template_version'] = 0
         self.__update_model_to_current()
 
-        return {"model": self.model}
+        return {"model": self.model, "diff": self.diff}
 
 
 class StochSSSpatialModel(StochSSBase):
